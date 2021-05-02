5ca5e72a2de556ad068c7ef1d11c6efb688fe8a8
Neil Macneale <neil.macneale.v@gmail.com>
2021-05-01 22:45:55 -0700

    Packet stream now uses a ringbuffer to read packets from a tcp stream

--- common/src/util/mod.rs
+++ common/src/util/mod.rs
@@ -1,3 +1,29 @@
 pub mod buffer;
 
 pub use buffer::{Buffer, BufferError};
+
+pub fn read_varint(buf: &[u8]) -> (i32, isize) {
+  let mut res: i32 = 0;
+  let mut total_read: isize = 0;
+  for i in 0..5 {
+    if i >= buf.len() {
+      // Incomplete varint
+      return (0, 0);
+    }
+    let read = buf[i];
+    if i == 4 && read & 0b10000000 != 0 {
+      // Invalid varint (read < 0 means invalid varint)
+      return (0, -1);
+    }
+
+    let v = read & 0b01111111;
+    res |= (v as i32) << (7 * i);
+
+    if read & 0b10000000 == 0 {
+      // Done reading bytes, so we set total read
+      total_read = i as isize + 1;
+      break;
+    }
+  }
+  (res, total_read)
+}
--- proxy/src/packet_stream.rs
+++ proxy/src/packet_stream.rs
@@ -1,32 +1,51 @@
 use crate::packet::Packet;
 
-use common::util::Buffer;
+use common::{util, util::Buffer};
+use ringbuf::{Consumer, Producer, RingBuffer};
 use std::io::Result;
 use tokio::{io::AsyncReadExt, net::TcpStream};
 
 pub struct Stream {
   stream: TcpStream,
-  buf: Buffer,
+  prod: Producer<u8>,
+  cons: Consumer<u8>,
 }
 
 impl Stream {
   pub fn new(stream: TcpStream) -> Self {
-    Stream { stream, buf: Buffer::new(vec![]) }
+    let buf = RingBuffer::new(1024);
+    let (prod, cons) = buf.split();
+    Stream { stream, prod, cons }
   }
 
   pub async fn poll(&mut self) -> Result<()> {
     let mut vec = vec![0u8; 256];
     let len = self.stream.read(&mut vec).await?;
     vec.truncate(len);
-    self.buf.append(vec);
+    self.prod.push_slice(&vec);
     Ok(())
   }
   pub fn read(&mut self) -> Result<Option<Packet>> {
-    let packet_len = self.buf.read_varint() as usize;
-    if packet_len > self.buf.len() {
+    let mut packet_len = 0;
+    let mut read = -1;
+    self.cons.access(|a, b| {
+      let (len, amount_read) = util::read_varint(a);
+      packet_len = len;
+      read = amount_read;
+    });
+    if read < 0 {
+      // TODO: -1 means invalid varint, so we should error here
       return Ok(None);
     }
-    Ok(Some(Packet::new(self.buf.read(packet_len))))
+    // Incomplete varint, or an incomplete packet
+    if read == 0 || packet_len as isize > self.cons.len() as isize {
+      return Ok(None);
+    }
+    // Now that we know we have a valid packet, we pop the length bytes
+    self.cons.discard(read as usize);
+    let mut vec = vec![0u8; packet_len as usize];
+    self.cons.pop_slice(&mut vec);
+    Ok(Some(Packet::new(vec)))
   }
   pub fn write(&self, p: Packet) {}
 }
