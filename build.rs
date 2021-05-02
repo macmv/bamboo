4061e332e2ec8cb6d8908e6ad333f887d40c7b03
Neil Macneale <neil.macneale.v@gmail.com>
2021-05-01 17:22:15 -0700

    Can compile protobufs and host a server

---
+++ build.rs
@@ -0,0 +1,13 @@
+use std::env;
+use std::path::PathBuf;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+  tonic_build::compile_protos("proto/connection.proto")?;
+
+  let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+  tonic_build::configure()
+    .file_descriptor_set_path(out_dir.join("connection.bin"))
+    .compile(&["proto/connection.proto"], &["proto"])
+    .unwrap();
+  Ok(())
+}
---
+++ src/main.rs
@@ -0,0 +1,66 @@
+use tokio::sync::mpsc;
+use tokio_stream::wrappers::ReceiverStream;
+use tonic::{transport::Server, Request, Response, Status, Streaming};
+
+pub mod proto {
+  tonic::include_proto!("connection");
+
+  pub(crate) const FILE_DESCRIPTOR_SET: &'static [u8] =
+    tonic::include_file_descriptor_set!("connection");
+}
+
+use proto::{
+  minecraft_server::{Minecraft, MinecraftServer},
+  Packet, ReserveSlotsRequest, ReserveSlotsResponse, StatusRequest, StatusResponse,
+};
+
+#[derive(Default)]
+pub struct ServerImpl {}
+
+#[tonic::async_trait]
+impl Minecraft for ServerImpl {
+  type ConnectionStream = ReceiverStream<Result<Packet, Status>>;
+  async fn connection(
+    &self,
+    req: Request<Streaming<Packet>>,
+  ) -> Result<Response<Self::ConnectionStream>, Status> {
+    let (tx, rx) = mpsc::channel(4);
+
+    dbg!(req);
+
+    tokio::spawn(async move {
+      let mut p = Packet::default();
+      p.id = 10;
+      println!("  => send {:?}", p);
+      tx.send(Ok(p)).await.unwrap();
+
+      println!(" /// done sending");
+    });
+
+    Ok(Response::new(ReceiverStream::new(rx)))
+  }
+  async fn status(&self, req: Request<StatusRequest>) -> Result<Response<StatusResponse>, Status> {
+    dbg!(req);
+    Ok(Response::new(StatusResponse::default()))
+  }
+  async fn reserve_slots(
+    &self,
+    req: Request<ReserveSlotsRequest>,
+  ) -> Result<Response<ReserveSlotsResponse>, Status> {
+    dbg!(req);
+    Ok(Response::new(ReserveSlotsResponse::default()))
+  }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+  let addr = "0.0.0.0:8483".parse().unwrap();
+
+  let svc = MinecraftServer::new(ServerImpl::default());
+  let descriptor = tonic_reflection::server::Builder::configure()
+    .register_encoded_file_descriptor_set(proto::FILE_DESCRIPTOR_SET)
+    .build()?;
+
+  Server::builder().add_service(svc).add_service(descriptor).serve(addr).await?;
+  Ok(())
+}
