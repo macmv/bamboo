f16d8a28f23ffd6630003009789ce127d04e3d2a
Neil Macneale <neil.macneale.v@gmail.com>
2021-05-05 16:01:40 -0700

    Added data for a lot of packets

--- common/src/net/cb.rs
+++ common/src/net/cb.rs
@@ -175,53 +175,74 @@ macro_rules! id_init {
 
 #[rustfmt::skip]
 fn create_empty(id: ID) -> proto::Packet {
+  // See https://wiki.vg/Protocol for more.
+  // In general, I list out each field in the order that it is used in the packet.
+  // If a packet has fields that look like this:
+  //   - int
+  //   - str
+  //   - int
+  // Then I would call `id_init!(ints: 2, strs: 1)`.
+  //
+  // For some packets, I just use a protobuf. See the comments on each packet for more.
+  //
+  // Lastly, there is conversion between versions of the game. For example, the keep alive
+  // packet changed from an int to a long. In general, I will go with the newer version,
+  // so that the latest client's features are entirely supported. However, a long for a keep
+  // alive id is ridiculous, so I just used an int in that case. All of the conversion to
+  // older packets is done on the proxy, so you should look at the implementation there for
+  // specifics about how this is done.
   match id {
-    ID::SpawnEntity               => id_init!(ints: 2, uuids: 1),
-    ID::SpawnExpOrb               => id_init!(),
-    ID::SpawnWeatherEntity        => id_init!(),
-    ID::SpawnLivingEntity         => id_init!(),
-    ID::SpawnPainting             => id_init!(),
-    ID::SpawnPlayer               => id_init!(),
-    ID::EntityAnimation           => id_init!(),
-    ID::Statistics                => id_init!(),
-    ID::AcknowledgePlayerDigging  => id_init!(),
-    ID::BlockBreakAnimation       => id_init!(),
-    ID::BlockEntityData           => id_init!(),
-    ID::BlockAction               => id_init!(),
-    ID::BlockChange               => id_init!(),
-    ID::BossBar                   => id_init!(),
-    ID::ServerDifficulty          => id_init!(),
-    ID::ChatMessage               => id_init!(),
-    ID::MultiBlockChange          => id_init!(),
-    ID::TabComplete               => id_init!(),
-    ID::DeclareCommands           => id_init!(),
-    ID::WindowConfirm             => id_init!(),
-    ID::CloseWindow               => id_init!(),
-    ID::WindowItems               => id_init!(),
-    ID::WindowProperty            => id_init!(),
-    ID::SetSlot                   => id_init!(),
-    ID::SetCooldown               => id_init!(),
-    ID::PluginMessage             => id_init!(),
-    ID::NamedSoundEffect          => id_init!(),
-    ID::Disconnect                => id_init!(),
-    ID::EntityStatus              => id_init!(),
-    ID::Explosion                 => id_init!(),
-    ID::UnloadChunk               => id_init!(),
-    ID::ChangeGameState           => id_init!(),
-    ID::OpenHorseWindow           => id_init!(),
+    ID::SpawnEntity               => id_init!(ints: 3, uuids: 1, doubles: 3, bytes: 2, shorts: 3),
+    ID::SpawnExpOrb               => id_init!(ints: 1, doubles: 3, shorts: 1),
+    ID::SpawnWeatherEntity        => id_init!(), // TODO: I think this packet was removed, idk
+    ID::SpawnLivingEntity         => id_init!(ints: 2, uuids: 1, doubles: 3, bytes: 3, shorts: 3),
+    ID::SpawnPainting             => id_init!(ints: 2, uuids: 1, positions: 1, bytes: 1),
+    ID::SpawnPlayer               => id_init!(ints: 1, uuids: 1, doubles: 3, bytes: 2),
+    ID::EntityAnimation           => id_init!(ints: 1, bytes: 1),
+    ID::Statistics                => id_init!(int_arrs: 3),
+    ID::AcknowledgePlayerDigging  => id_init!(positions: 1, ints: 2, bools: 1),
+    ID::BlockBreakAnimation       => id_init!(ints: 1, positions: 1, bytes: 1),
+    ID::BlockEntityData           => id_init!(positions: 1, bytes: 1, nbt_tags: 1),
+    ID::BlockAction               => id_init!(positions: 1, bytes: 2, ints: 1),
+    ID::BlockChange               => id_init!(positions: 1, ints: 1),
+    ID::BossBar                   => id_init!(uuids: 1, ints: 1), // TODO: Custom type here
+    ID::ServerDifficulty          => id_init!(bytes: 1, bools: 1),
+    ID::ChatMessage               => id_init!(strs: 1, bytes: 1, uuids: 1),
+    ID::MultiBlockChange          => id_init!(positions: 1, bools: 1, ints: 1, byte_arrs: 1),
+    ID::TabComplete               => id_init!(ints: 3), // TODO: Custom type here
+    ID::DeclareCommands           => id_init!(ints: 2, byte_arrs: 1),
+    ID::WindowConfirm             => id_init!(bytes: 1, shorts: 1, bools: 1),
+    ID::CloseWindow               => id_init!(bytes: 1),
+    ID::WindowItems               => id_init!(bytes: 1, shorts: 1), // TODO: Setup slot type
+    ID::WindowProperty            => id_init!(bytes: 1, shorts: 2),
+    ID::SetSlot                   => id_init!(bytes: 1, shorts: 1), // TODO: Setup slot type
+    ID::SetCooldown               => id_init!(ints: 2),
+    ID::PluginMessage             => id_init!(strs: 1, byte_arrs: 1),
+    // There is an extra int here, which should be used to set the sound id.
+    // This is for backwards compatibility, as older clients do not use a sound by name.
+    ID::NamedSoundEffect          => id_init!(strs: 1, ints: 5, floats: 2),
+    ID::Disconnect                => id_init!(strs: 1),
+    ID::EntityStatus              => id_init!(ints: 1, bytes: 1),
+    ID::Explosion                 => id_init!(floats: 7, byte_arrs: 1),
+    ID::UnloadChunk               => id_init!(ints: 2),
+    ID::ChangeGameState           => id_init!(bytes: 1, floats: 1),
+    ID::OpenHorseWindow           => id_init!(bytes: 1, ints: 2), // TODO: Find out more about this packet
+    // Newer clients use a long, which is ridiculous. Just cast this on newer clients.
     ID::KeepAlive                 => id_init!(ints: 1),
-    ID::ChunkData                 => id_init!(),
-    ID::Effect                    => id_init!(),
-    ID::Particle                  => id_init!(),
-    ID::UpdateLight               => id_init!(),
-    ID::JoinGame                  => id_init!(),
-    ID::MapData                   => id_init!(),
-    ID::TradeList                 => id_init!(),
-    ID::EntityPosition            => id_init!(),
-    ID::EntityPositionAndRotation => id_init!(),
-    ID::EntityRotation            => id_init!(),
-    ID::EntityOnGround            => id_init!(),
-    ID::VehicleMove               => id_init!(),
+    // This should be its own type. It changes so much that relying on int arrays is too difficult.
+    ID::ChunkData                 => id_init!(), // TODO: Custom type here
+    ID::Effect                    => id_init!(ints: 2, positions: 1, bools: 1),
+    ID::Particle                  => id_init!(ints: 2, bools: 1, doubles: 3, floats: 4, byte_arrs: 1),
+    // Only used on newer versions. For older clients, light data is sent with ChunkData.
+    ID::UpdateLight               => id_init!(ints: 6, bools: 1, byte_arrs: 2),
+    ID::JoinGame                  => id_init!(ints: 3, bools: 5, bytes: 1, str_arrs: 1, nbt_tags: 2, strs: 1, longs: 1),
+    ID::MapData                   => id_init!(), // TODO: Custom proto
+    ID::TradeList                 => id_init!(ints: 3, bools: 2), // TODO: Custom proto
+    ID::EntityPosition            => id_init!(ints: 1, shorts: 3, bools: 1),
+    ID::EntityPositionAndRotation => id_init!(ints: 1, shorts: 3, bytes: 2, bools: 1),
+    ID::EntityRotation            => id_init!(ints: 1, bytes: 2, bools: 1),
+    ID::EntityOnGround            => id_init!(ints: 1, bools: 1),
+    ID::VehicleMove               => id_init!(doubles: 3, floats: 2),
     ID::OpenBook                  => id_init!(),
     ID::OpenWindow                => id_init!(),
     ID::OpenSignEditor            => id_init!(),
