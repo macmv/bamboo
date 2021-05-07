d9e773b699ebc206b8f1ea40b13e90382289bd05
Neil Macneale <neil.macneale.v@gmail.com>
2021-05-06 18:06:29 -0700

    Replaced most of the code in other.rs with a macro call, so that the list of types is only specified once

--- common/src/net/other.rs
+++ common/src/net/other.rs
@@ -2,12 +2,6 @@ use crate::proto;
 use prost::{DecodeError, EncodeError, Message};
 use prost_types::Any;
 
-#[derive(Debug)]
-pub enum Other {
-  Chunk(proto::Chunk),
-  BossBar(proto::BossBar),
-}
-
 /// Creates a type url from the given keyword. This should be used to
 /// endcode/decode Any types.
 macro_rules! create_type_url {
@@ -16,44 +10,49 @@ macro_rules! create_type_url {
   };
 }
 
-macro_rules! any_decode {
-  [$any: expr, $($val: ident),*] => {
-    match $any.type_url.as_str() {
+macro_rules! generate_other {
+  [$($val: ident),*] => {
+    #[derive(Debug)]
+    pub enum Other {
+      // Generates this:
+      // Chunk(proto::Chunk),
+      // BossBar(proto::BossBar),
       $(
-        create_type_url!($val) => {
-          match proto::$val::decode($any.value.as_slice()) {
-            Ok(msg) => Ok(Other::$val(msg)),
-            Err(e) => Err(e),
-          }
-        },
-      )*
-      _ => panic!("unknown type {}", $any.type_url),
-    }
-  }
-}
-
-macro_rules! any_encode {
-  [$self: expr, $buf: expr, $($val: ident),*] => {
-    match $self {
-      $(
-        Self::$val(pb) => match pb.encode($buf) {
-          Ok(_) => Ok(create_type_url!($val)),
-          Err(e) => Err(e),
-        },
+        $val(proto::$val),
       )*
     }
-  }
+
+    impl Other {
+      pub fn from_any(pb: Any) -> Result<Self, DecodeError> {
+        match pb.type_url.as_str() {
+          // Generates a match statement for each type url in Other
+          $(
+            create_type_url!($val) => {
+              match proto::$val::decode(pb.value.as_slice()) {
+                Ok(msg) => Ok(Other::$val(msg)),
+                Err(e) => Err(e),
+              }
+            },
+          )*
+          _ => panic!("unknown type {}", pb.type_url),
+        }
+      }
+      pub fn to_any(&self) -> Result<Any, EncodeError> {
+        let mut b = bytes::BytesMut::new();
+        let name = match self {
+          // Generates a match statement for each type in Other.
+          // This will always be exhaustive.
+          $(
+            Self::$val(pb) => {
+              pb.encode(&mut b)?;
+              create_type_url!($val)
+            },
+          )*
+        };
+        Ok(Any { type_url: name.to_string(), value: b.to_vec() })
+      }
+    }
+  };
 }
 
-impl Other {
-  pub fn from_any(pb: Any) -> Result<Self, DecodeError> {
-    any_decode![pb, Chunk, BossBar]
-  }
-  pub fn to_any(&self) -> Result<Any, EncodeError> {
-    let mut b = bytes::BytesMut::new();
-    let name = any_encode![self, &mut b, Chunk, BossBar]?;
-
-    dbg!(create_type_url!(Chunk));
-    Ok(Any { type_url: name.to_string(), value: b.to_vec() })
-  }
-}
+generate_other![Chunk, BossBar];
