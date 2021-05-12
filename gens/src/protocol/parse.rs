5dfb786e8feaea6bfe79db8970adf3bc92a47b31
Neil Macneale <neil.macneale.v@gmail.com>
2021-05-11 21:39:24 -0700

    Can parse all builtin fields, but cannot parse things like particleData

--- gens/src/protocol/parse.rs
+++ gens/src/protocol/parse.rs
@@ -1,14 +1,125 @@
 use super::{Packet, Version};
+use serde::de::{self, Deserialize, Deserializer, SeqAccess, Visitor};
 use serde_derive::Deserialize;
-use std::{collections::HashMap, error::Error, fs, path::Path};
+use std::{collections::HashMap, error::Error, fmt, fs, path::Path};
 
 type JsonTypeMap = HashMap<String, JsonType>;
-type JsonType = Vec<JsonTypeField>;
 
+#[derive(Debug)]
+struct JsonType {
+  kind:  String,
+  value: Box<JsonTypeValue>,
+}
+
+impl<'de> Deserialize<'de> for JsonType {
+  fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+  where
+    D: Deserializer<'de>,
+  {
+    struct Arr;
+
+    macro_rules! match_values {
+      ($self:expr, $k:expr, $s:expr, [$($name:expr, $kind:ident),*]) => {
+        match $k {
+          $(
+            $name => Ok(JsonTypeValue::$kind($s.next_element()?.ok_or_else(|| de::Error::invalid_length(1, $self))?)),
+          )*
+          v => Err(de::Error::unknown_variant(v, &[$($name),*]))
+        }
+      };
+    }
+
+    impl<'de> Visitor<'de> for Arr {
+      type Value = JsonType;
+
+      fn expecting(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+        write!(f, "a string")
+      }
+
+      fn visit_str<E>(self, mut s: &str) -> Result<Self::Value, E>
+      where
+        E: de::Error,
+      {
+        Ok(JsonType { kind: "direct".into(), value: Box::new(JsonTypeValue::Direct(s.into())) })
+      }
+
+      fn visit_seq<S>(self, mut s: S) -> Result<Self::Value, S::Error>
+      where
+        S: SeqAccess<'de>,
+      {
+        let kind = s.next_element()?.ok_or_else(|| de::Error::invalid_length(0, &self))?;
+        let value = match_values!(
+          &self,
+          kind,
+          s,
+          [
+            "container",
+            Container,
+            "array",
+            Array,
+            "mapper",
+            Mapper,
+            "switch",
+            Switch,
+            "buffer",
+            Buffer,
+            "option",
+            Option,
+            "bitfield",
+            Bitfield
+          ]
+        )?;
+        Ok(JsonType { kind: kind.into(), value: Box::new(value) })
+      }
+    }
+
+    deserializer.deserialize_any(Arr)
+  }
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(untagged)]
+enum JsonTypeValue {
+  // Just a string type (will be something like "varint")
+  Direct(String),
+  // A container. This is a list of objects
+  Container(Vec<JsonContainer>),
+  // This is a list of fields, with a count type
+  Array(JsonArray),
+  // This is a list of mappings. TODO: Learn more
+  Mapper(JsonMapper),
+  // This is a list of conditionals and values. TODO: Learn more
+  Switch(JsonSwitch),
+  // This is a byte array, with a length type
+  Buffer(JsonBuffer),
+  // This is a value that may or may not exist
+  Option(Option<JsonType>),
+  // This is a value that may or may not exist
+  Bitfield(Vec<JsonBitfield>),
+}
+
+#[derive(Debug, Deserialize)]
+struct JsonBitfield {
+  name:   String,
+  size:   u32,
+  signed: bool,
+}
+
+#[derive(Debug, Deserialize)]
+struct JsonContainer {
+  name: String,
+  #[serde(alias = "type")]
+  ty:   JsonType,
+}
+
+// If count_type is none, then it is a fixed length array of length count. If
+// count is none, then this is an array prefixed with a value of the given type.
+// If both are none, this is invalid.
 #[derive(Debug, Deserialize)]
 struct JsonArray {
   #[serde(alias = "countType")]
-  count_type: String,
+  count_type: Option<JsonType>,
+  count:      Option<u32>,
   #[serde(alias = "type")]
   ty:         JsonType,
 }
@@ -16,26 +127,37 @@ struct JsonArray {
 #[derive(Debug, Deserialize)]
 struct JsonObject {
   name: String,
+  #[serde(alias = "type")]
+  ty:   JsonType,
 }
 
 #[derive(Debug, Deserialize)]
 struct JsonAnonObject {
   anon: bool,
+  #[serde(alias = "type")]
+  ty:   JsonType,
 }
 
 #[derive(Debug, Deserialize)]
-#[serde(untagged)]
-enum JsonTypeField {
-  // This is "container" or "array". We ignore it, as the type is infered because enums
-  Kind(String),
-  // A container. This is a list of objects
-  Container(JsonType),
-  // This is a list of fields, with a count type
-  Array(JsonArray),
-  // This is an actual object
-  Object(JsonObject),
-  // This is an actual object
-  AnonObject(JsonAnonObject),
+struct JsonMapper {
+  mappings: HashMap<String, String>,
+  #[serde(alias = "type")]
+  ty:       JsonType,
+}
+
+#[derive(Debug, Deserialize)]
+struct JsonSwitch {
+  // Another field name to be compared with
+  #[serde(alias = "compareTo")]
+  compare_to: String,
+  fields:     HashMap<String, JsonType>,
+}
+
+#[derive(Debug, Deserialize)]
+struct JsonBuffer {
+  // The type that the length is in
+  #[serde(alias = "countType")]
+  count_type: String,
 }
 
 #[derive(Debug, Deserialize)]
