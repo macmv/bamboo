0ce809212e59caac164c5fc9e15b1dd5c82573fc
Neil Macneale <neil.macneale.v@gmail.com>
2021-05-26 22:48:13 -0700

    Using default implementations, I can simplify custom biomes a lot

--- server/src/world/gen/forest.rs
+++ server/src/world/gen/forest.rs
@@ -16,40 +16,11 @@ impl Gen {
 }
 
 impl BiomeGen for Gen {
-  fn fill_chunk(&self, world: &WorldGen, pos: ChunkPos, c: &mut MultiChunk) {
-    // This is the height at the middle of the chunk. It is a good average height
-    // for the whole chunk.
-    let average_stone_height = world.height_at(pos.block() + Pos::new(8, 0, 8)) as i32 - 5;
-    c.fill_kind(Pos::new(0, 0, 0), Pos::new(15, average_stone_height, 15), block::Kind::Stone)
-      .unwrap();
+  fn decorate(&self, world: &WorldGen, pos: ChunkPos, c: &mut MultiChunk) {
     for x in 0..16 {
       for z in 0..16 {
-        let height = world.height_at(pos.block() + Pos::new(x, 0, z)) as i32;
-        let stone_height = height - 5;
-        match stone_height.cmp(&average_stone_height) {
-          Ordering::Less => {
-            c.fill_kind(
-              Pos::new(x, stone_height + 1, z),
-              Pos::new(x, average_stone_height, z),
-              block::Kind::Air,
-            )
-            .unwrap();
-          }
-          Ordering::Greater => {
-            c.fill_kind(
-              Pos::new(x, average_stone_height, z),
-              Pos::new(x, stone_height, z),
-              block::Kind::Stone,
-            )
-            .unwrap();
-          }
-          _ => {}
-        }
-        c.fill_kind(Pos::new(x, height - 4, z), Pos::new(x, height - 1, z), block::Kind::Dirt)
-          .unwrap();
-        c.set_kind(Pos::new(x, height, z), block::Kind::Grass).unwrap();
-        // Trees
         let p = pos.block() + Pos::new(x, 0, z);
+        let height = self.height_at(world, p);
         if self.trees.contains(p.x(), p.z()) {
           c.fill_kind(Pos::new(x, height + 1, z), Pos::new(x, height + 4, z), block::Kind::Stone)
             .unwrap();
--- server/src/world/gen/mod.rs
+++ server/src/world/gen/mod.rs
@@ -7,8 +7,116 @@ use std::cmp::Ordering;
 mod desert;
 mod forest;
 
+pub struct BiomeLayers {
+  layers:       Vec<(block::Kind, u32)>,
+  main_area:    block::Kind,
+  total_height: u32, // Sum of layers
+}
+
+impl BiomeLayers {
+  /// Creates a new biome layer definition. The main area should probably be
+  /// stone; it is what will be used to fill all the blocks under the actual
+  /// layers.
+  pub fn new(main_area: block::Kind) -> Self {
+    BiomeLayers { main_area, layers: vec![], total_height: 0 }
+  }
+
+  /// Adds a new layer. This will be placed ontop of the previous layer.
+  ///
+  /// # Example
+  ///
+  /// This would create the default block layers you would see in a forest or
+  /// plains.
+  ///
+  /// ```rust
+  /// let mut layers = BiomeLayers::new(block::Kind::Stone);
+  /// layers.add(block::Kind::Dirt, 4);
+  /// layers.add(block::Kind::GrassBlock, 1);
+  /// ```
+  pub fn add(&mut self, kind: block::Kind, height: u32) {
+    self.layers.push((kind, height));
+    self.total_height += height;
+  }
+
+  /// Returns the layer at a given index.
+  pub fn get(&self, i: usize) -> Option<&(block::Kind, u32)> {
+    self.layers.get(i)
+  }
+
+  /// Returns the total height of all defined layers.
+  pub fn total_height(&self) -> u32 {
+    self.total_height
+  }
+}
+
 pub trait BiomeGen {
-  fn fill_chunk(&self, world: &WorldGen, pos: ChunkPos, chunk: &mut MultiChunk);
+  /// This fills an entire chunk with the given biome. This will fill the chunk
+  /// with stone, up to the height at the middle. It will then carve/add blocks
+  /// to the other columns of the chunk. Finally, it will call [`fill_column`]
+  /// for each column within the chunk. It will use the height of the stone as
+  /// the minimum to pass to `fill_column`.
+  ///
+  /// For most biomes, this should not be overriden. [`height_at`] should be
+  /// overriden if you want to build something like a mountain, and [`layers`]
+  /// should be overriden if you need something like a desert.
+  fn fill_chunk(&self, world: &WorldGen, pos: ChunkPos, c: &mut MultiChunk) {
+    let average_min_height = self.height_at(world, pos.block() + Pos::new(8, 0, 8));
+    let layers = self.layers();
+    c.fill_kind(Pos::new(0, 0, 0), Pos::new(15, average_min_height, 15), layers.main_area).unwrap();
+    for x in 0..16 {
+      for z in 0..16 {
+        let height = world.height_at(pos.block() + Pos::new(x, 0, z)) as i32;
+        let min_height = height - layers.total_height() as i32;
+        match min_height.cmp(&average_min_height) {
+          Ordering::Less => {
+            c.fill_kind(
+              Pos::new(x, min_height + 1, z),
+              Pos::new(x, average_min_height, z),
+              block::Kind::Air,
+            )
+            .unwrap();
+          }
+          Ordering::Greater => {
+            c.fill_kind(
+              Pos::new(x, average_min_height, z),
+              Pos::new(x, min_height, z),
+              layers.main_area,
+            )
+            .unwrap();
+          }
+          _ => {}
+        }
+        let mut level = min_height as u32;
+        for (k, depth) in &layers.layers {
+          c.fill_kind(Pos::new(x, level as i32, z), Pos::new(x, (level + depth) as i32, z), *k)
+            .unwrap();
+          level += depth;
+        }
+      }
+    }
+  }
+  /// Decorates the given chunk. This is called for every chunk where a block of
+  /// this biome is in the radius of [`decorate_radius`].
+  fn decorate(&self, _world: &WorldGen, _pos: ChunkPos, _c: &mut MultiChunk) {}
+  /// Returns the longest distance that decorations can extend outside of this
+  /// biome. This is used to call [`decorate`] when there are blocks of this
+  /// biome in a nearby chunk.
+  fn decorate_radius(&self) -> u32 {
+    0
+  }
+  /// Returns the layers that should be used to fill in the ground. See
+  /// [`BiomeLayer`] for more.
+  fn layers(&self) -> BiomeLayers {
+    let mut layers = BiomeLayers::new(block::Kind::Stone);
+    layers.add(block::Kind::Dirt, 4);
+    layers.add(block::Kind::Grass, 4);
+    layers
+  }
+  /// Returns this biome's height at the given position. By default, this just
+  /// uses the world height at the given position.
+  fn height_at(&self, world: &WorldGen, pos: Pos) -> i32 {
+    world.height_at(pos) as i32
+  }
 }
 
 pub struct WorldGen {
