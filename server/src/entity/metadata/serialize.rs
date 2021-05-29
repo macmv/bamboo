d14c8b6aae4adef2b08535b3375f14cea4c1570e
Neil Macneale <neil.macneale.v@gmail.com>
2021-05-29 15:45:44 -0700

    Added some setters

--- server/src/entity/metadata/serialize.rs
+++ server/src/entity/metadata/serialize.rs
@@ -1,4 +1,5 @@
 use super::{Field, Metadata};
+use crate::item;
 use common::version::ProtocolVersion;
 use std::{error::Error, fmt};
 
@@ -46,13 +47,29 @@ impl Metadata {
     }
     self.set_field(index, Field::Varint(value))
   }
+  /// Adds the given bool to the metadata. Valid for 1.9+.
+  pub fn set_bool(&mut self, index: u8, value: bool) -> Result<(), MetadataError> {
+    if self.ver < ProtocolVersion::V1_9 {
+      return Err(MetadataError::Version(self.ver));
+    }
+    self.set_field(index, Field::Bool(value))
+  }
+
   /// Adds the given byte to the metadata. Valid for all versions.
   pub fn set_byte(&mut self, index: u8, value: u8) -> Result<(), MetadataError> {
     self.set_field(index, Field::Byte(value))
   }
-  /// Adds the give float to the metadata. Valid for all versions.
+  /// Adds the given float to the metadata. Valid for all versions.
   pub fn set_float(&mut self, index: u8, value: f32) -> Result<(), MetadataError> {
     self.set_field(index, Field::Float(value))
+  }
+  /// Adds the given string to the metadata. Valid for all versions.
+  pub fn set_str(&mut self, index: u8, value: String) -> Result<(), MetadataError> {
+    self.set_field(index, Field::String(value))
+  }
+  /// Adds the given item stack to the metadata. Valid for all versions.
+  pub fn set_item(&mut self, index: u8, value: item::Stack) -> Result<(), MetadataError> {
+    self.set_field(index, Field::Item(value))
   }
 
   fn set_field(&mut self, index: u8, value: Field) -> Result<(), MetadataError> {
