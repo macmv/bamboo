5f19e40c42306518a600650f80a993f293c44779
Neil Macneale <neil.macneale.v@gmail.com>
2021-06-03 14:11:29 -0700

    Documented all command functionality

--- server/src/command/mod.rs
+++ server/src/command/mod.rs
@@ -1,7 +1,32 @@
+//! This package handles commands. The [`Command`] type can be used to declare
+//! a command. The arguments on that command tell both the server and the client
+//! how it should parse/autocomplete commands.
+//!
+//! # Examples
+//!
+//! ```
+//! let mut c = Command::new("fill");
+//! c.add_arg("first", Parser::BlockPos); // The string is just the name of the argument. It serves no purpose in parsing.
+//! c.add_arg("second", Parser::BlockPos);
+//! c.add_arg("block", Parser::BlockState);
+//! ```
+//!
+//! This would create a fill command, which would take three arguments: two
+//! block positions, and a block type. The block positions are 3 numbers
+//! seperated by spaces. However, they also support relative coordinates: `~10`
+//! means 10 blocks up/right/forward of your current position. See the
+//! [`Parser`] type for details on the various parsers.
 mod parser;
 
 pub use parser::Parser;
 
+/// A single command. This can be used to construct an entire command. However,
+/// it is also used to represent an argument of a command. When you call
+/// [`add_arg`](Self::add_arg) or [`add_lit`](Self::add_lit), these functions
+/// will add a new argument to the command. But they will also return a command.
+/// This is a seperate struct, and is a reference into the command this was
+/// called on. This makes chaining [`add_arg`](Self::add_arg) add arguments
+/// one-after-another.
 #[derive(Debug, Clone)]
 pub struct Command {
   name:     String,
@@ -16,20 +41,62 @@ enum NodeType {
 }
 
 impl Command {
+  /// Creates a new command. This should be used when you want an entirely new
+  /// command (not an argument of another command).
   pub fn new(name: &str) -> Self {
     Self::lit(name.into())
   }
+  /// Creates a new literal node. Use [`add_lit`](Self::add_lit) if you want to
+  /// add a literal node to the current command.
   fn lit(name: String) -> Self {
     Command { name, ty: NodeType::Literal, children: vec![] }
   }
+  /// Creates a new argument node. Use [`add_arg`](Self::add_arg) if you want to
+  /// add an argument node to the current command.
   fn arg(name: String, parser: Parser) -> Self {
     Command { name, ty: NodeType::Argument(parser), children: vec![] }
   }
+  /// Adds a new literal argument to the command. Unlike
+  /// [`add_arg`](Self::add_arg), the name has a purpose in parsing here.
+  /// Literal arguments match the exact text of the name in a command. For
+  /// example, you might have a command that works like this:
+  ///
+  /// ```plain
+  /// /fill rect ~ ~ ~ ~20 ~ ~20 minecraft:dirt
+  /// /fill circle ~ ~ ~ 10.0 minecraft:stone
+  /// ```
+  ///
+  /// And you would implement that command like this:
+  ///
+  /// ```
+  /// let mut c = Command::new("fill");
+  /// c.add_lit("rect")
+  ///   .add_arg("min", Parser::BlockPos)
+  ///   .add_arg("max", Parser::BlockPos)
+  ///   .add_arg("block", Parser::BlockState);
+  /// c.add_lit("circle")
+  ///   .add_arg("pos", Parser::BlockPos)
+  ///   .add_arg("radius", Parser::Float { min: Some(0.0), max: None })
+  ///   .add_arg("block", Parser::BlockState);
+  /// ```
+  ///
+  /// Note that the `&mut Command` returned by this function is not a refernce
+  /// to `self`. It is a new struct, created by this function. It is returned so
+  /// that you can chain arguments, and add multiple arguments after each other
+  /// easily.
   pub fn add_lit(&mut self, name: &str) -> &mut Command {
     self.children.push(Command::lit(name.into()));
     let index = self.children.len() - 1;
     self.children.get_mut(index).unwrap()
   }
+  /// Adds a new argument to this command. Unlike literal arguments, the name
+  /// does not matter here. It will only be show to the client when they are
+  /// autocompleting the command. Other than that, it is never used. The
+  /// important part about this function is the parser. Each parser will match a
+  /// certain amount of text. They range from a single float, to an entity
+  /// pattern (things like `@a` or `@e[type=Skeleton]`), to a block name
+  /// (`minecraft:bedrock`). See the [`Parser`] type for details about every
+  /// parser.
   pub fn add_arg(&mut self, name: &str, parser: Parser) -> &mut Command {
     self.children.push(Command::arg(name.into(), parser));
     let index = self.children.len() - 1;
@@ -57,6 +124,5 @@ mod tests {
       .add_arg("radius", Parser::Float { min: Some(0.0), max: None })
       .add_arg("block", Parser::BlockState);
     dbg!(c);
-    panic!();
   }
 }
--- server/src/command/parser.rs
+++ server/src/command/parser.rs
@@ -1,58 +1,120 @@
+/// A string parsing type. Used only in [`Parser::String`].
 #[derive(Debug, Clone)]
 pub enum StringType {
+  /// Matches a single word.
   SingleWord,
+  /// Matches either a single word, or a phrase in double quotes. Quotes can be
+  /// inserted in the string with `\"`.
   QuotablePhrase,
+  /// Matches all remaining text in the command. Quotes are not interpreted.
   GreedyPhrase,
 }
 
+/// This is a command argument parser. All of the information for this comes
+/// from [wiki.vg](https://wiki.vg/Command_Data). They have a great collection
+/// of data for all of this stuff, and this entire server wouldn't be possible
+/// without them.
 #[derive(Debug, Clone)]
 pub enum Parser {
-  Bool,                                           // True or false
-  Double { min: Option<f64>, max: Option<f64> },  // A double with optional min and max
-  Float { min: Option<f32>, max: Option<f32> },   // A float with optional min and max
-  Integer { min: Option<i32>, max: Option<i32> }, // An int with optional min and max
-  String(StringType),                             // A string
-  Entity { single: bool, players: bool },         // An entity. Can be things like @e, or username
-  ScoreHolder { multiple: bool },                 // A user that is on the current scoreboard
+  // Simple types:
+  /// True or false.
+  Bool,
+  /// A double, with optional min and max values.
+  Double { min: Option<f64>, max: Option<f64> },
+  /// A float, with optional min and max values.
+  Float { min: Option<f32>, max: Option<f32> },
+  /// An int, with optional min and max values.
+  Integer { min: Option<i32>, max: Option<i32> },
+  /// A string. See [`StringType`] for details on how this is parsed.
+  String(StringType),
+  /// An entity. If `single` is set, then this can only match one entity (things
+  /// like `@e` or `@a` are not allowed). If players is set, then matching
+  /// players (with either a username or `@a`) is allowed.
+  Entity { single: bool, players: bool },
+  /// A user that is on the current scoreboard. With the scoreboard system that
+  /// sugarcane has, this doesn't make that much sense.
+  ScoreHolder { multiple: bool },
 
-  GameProfile,              // Player, online or not. Can also use a selector.
-  BlockPos,                 // location, represented as 3 numbers (which must be integers)
-  ColumnPos,                // column location, represented as 3 numbers (which must be integers)
-  Vec3,                     // A location, represented as 3 numbers
-  Vec2,                     // A location, represented as 2 numbers
-  BlockState,               // A block state, optionally including NBT and state information.
-  BlockPredicate,           // A block, or a block tag.
-  ItemStack,                // An item, optionally including NBT.
-  ItemPredicate,            // An item, or an item tag.
-  Color,                    // Chat color. One of the names from Chat#Colors, or reset.
-  Component,                // A JSON Chat component.
-  Message,                  // A regular message, potentially including selectors.
-  Nbt,                      // An NBT value, parsed using JSON-NBT rules.
-  NbtPath,                  // A path within an NBT value, allowing for array and member accesses.
-  Objective,                // A scoreboard objective.
-  ObjectiveCriteria,        // A single score criterion.
-  Operation,                // A scoreboard operator.
-  Particle,                 // A particle effect
-  Rotation,                 // angle, represented as 2 floats
-  Angle,                    // A single float
-  ScoreboardSlot,           // Scoreboard display position slot. list, sidebar, belowName, etc
-  Swizzle,                  // A collection of up to 3 axes.
-  Team,                     // The name of a team. Parsed as an unquoted string.
-  ItemSlot,                 // A name for an inventory slot.
-  ResourceLocation,         // An Identifier.
-  MobEffect,                // A potion effect.
-  Function,                 // A function.
-  EntityAnchor,             // entity anchor related to the facing argument
-  Range { decimals: bool }, // A range of values with a min and a max.
-  IntRange,                 // An integer range of values with a min and a max.
-  FloatRange,               // A floating-point range of values with a min and a max.
-  ItemEnchantment,          // Represents a item enchantment.
-  EntitySummon,             // Represents an entity summon.
-  Dimension,                // Represents a dimension.
-  Uuid,                     // Represents a UUID value.
-  NbtTag,                   // Represents a partial nbt tag, usable in data modify command.
-  NbtCompoundTag,           // Represents a full nbt tag.
-  Time,                     // Represents a time duration.
-  Modid,                    // A forge mod id
-  Enum,                     // A enum class to use for suggestion. Added by Minecraft Forge.
+  /// Player, online or not. Can also use a selector.
+  GameProfile,
+  /// location, represented as 3 numbers (which must be integers)
+  BlockPos,
+  /// column location, represented as 3 numbers (which must be integers)
+  ColumnPos,
+  /// A location, represented as 3 numbers
+  Vec3,
+  /// A location, represented as 2 numbers
+  Vec2,
+  /// A block state, optionally including NBT and state information.
+  BlockState,
+  /// A block, or a block tag.
+  BlockPredicate,
+  /// An item, optionally including NBT.
+  ItemStack,
+  /// An item, or an item tag.
+  ItemPredicate,
+  /// Chat color. One of the names from Chat#Colors, or reset.
+  Color,
+  /// A JSON Chat component.
+  Component,
+  /// A regular message, potentially including selectors.
+  Message,
+  /// An NBT value, parsed using JSON-NBT rules.
+  Nbt,
+  /// A path within an NBT value, allowing for array and member accesses.
+  NbtPath,
+  /// A scoreboard objective.
+  Objective,
+  /// A single score criterion.
+  ObjectiveCriteria,
+  /// A scoreboard operator.
+  Operation,
+  /// A particle effect
+  Particle,
+  /// angle, represented as 2 floats
+  Rotation,
+  /// A single float
+  Angle,
+  /// Scoreboard display position slot. list, sidebar, belowName, etc
+  ScoreboardSlot,
+  /// A collection of up to 3 axes.
+  Swizzle,
+  /// The name of a team. Parsed as an unquoted string.
+  Team,
+  /// A name for an inventory slot.
+  ItemSlot,
+  /// An Identifier.
+  ResourceLocation,
+  /// A potion effect.
+  MobEffect,
+  /// A function.
+  Function,
+  /// entity anchor related to the facing argument
+  EntityAnchor,
+  /// A range of values with a min and a max.
+  Range { decimals: bool },
+  /// An integer range of values with a min and a max.
+  IntRange,
+  /// A floating-point range of values with a min and a max.
+  FloatRange,
+  /// Represents a item enchantment.
+  ItemEnchantment,
+  /// Represents an entity summon.
+  EntitySummon,
+  /// Represents a dimension.
+  Dimension,
+  /// Represents a UUID value.
+  Uuid,
+  /// Represents a partial nbt tag, usable in data modify command.
+  NbtTag,
+  /// Represents a full nbt tag.
+  NbtCompoundTag,
+  /// Represents a time duration.
+  Time,
+
+  // Forge only types:
+  /// A forge mod id
+  Modid,
+  /// A enum class to use for suggestion. Added by Minecraft Forge.
+  Enum,
 }
