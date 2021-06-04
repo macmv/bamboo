b11c3312e3a1e572b5f6cba77d82fa627ed68ac4
Neil Macneale <neil.macneale.v@gmail.com>
2021-06-04 13:45:20 -0700

    File names get sorted correctly

--- data-gen/src/util/mod.rs
+++ data-gen/src/util/mod.rs
@@ -15,13 +15,24 @@ pub fn load_versions(p: &Path, name: &str) -> Result<Vec<PathBuf>, Box<dyn Error
     let named_path = path.join(name);
     if named_path.exists() {
       let fname = path.file_name().unwrap().to_str().unwrap();
-      // We use 1.13.2 instead of 1.13, and 1.7 is different so I haven't bothered to
-      // parse it.
-      if fname == "1.13" || fname == "1.7" || fname.chars().any(|c| c.is_ascii_lowercase()) {
+      // We use 1.16.2 instead of 1.16.1, 1.13.2 instead of 1.13, and 1.7 is different
+      // so I haven't bothered to parse it.
+      if fname == "1.16.1"
+        || fname == "1.13"
+        || fname == "1.7"
+        || fname.chars().any(|c| c.is_ascii_lowercase())
+      {
         continue;
       }
       versions.push(named_path);
     }
   }
+  versions.sort_by(|a, b| {
+    let a_name = a.parent().unwrap().file_name().unwrap().to_str().unwrap();
+    let b_name = b.parent().unwrap().file_name().unwrap().to_str().unwrap();
+    let a_id = a_name.split('.').nth(1).unwrap().parse::<i32>().unwrap();
+    let b_id = b_name.split('.').nth(1).unwrap().parse::<i32>().unwrap();
+    b_id.cmp(&a_id) // We want them in reverse order
+  });
   Ok(versions)
 }
