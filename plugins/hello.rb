852dd33b492ec62b81da244805a3b178a2fae54d
Neil Macneale <neil.macneale.v@gmail.com>
2021-06-05 19:10:17 -0700

    Added sample plugin

---
+++ plugins/hello.rb
@@ -0,0 +1,16 @@
+module Hello
+  BIG = "gaming"
+
+  def self.init
+    puts "Loaded hello plugin"
+    Sugarcane::broadcast("big gaming")
+  end
+
+  def self.on_block_place(player, pos)
+    Sugarcane::broadcast("#{player.username} just placed a block at #{pos}")
+  end
+end
+
+p Hello::public_methods - Module::public_methods
+
+Hello
