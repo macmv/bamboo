eda317e7625754cf41e2618673f56a54030bf514
Neil Macneale <neil.macneale.v@gmail.com>
2021-06-07 17:36:53 -0700

    Changed to log4rs

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -3,7 +3,12 @@
   test(no_crate_inject, attr(deny(warnings)))
 )]
 
-use flexi_logger::{Duplicate, LogTarget, Logger};
+// use flexi_logger::{Duplicate, LogTarget, Logger};
+use log::LevelFilter;
+use log4rs::append::console::ConsoleAppender;
+use log4rs::append::file::FileAppender;
+use log4rs::config::{Appender, Config, Logger, Root};
+use log4rs::encode::pattern::PatternEncoder;
 
 pub mod math;
 pub mod net;
@@ -21,11 +26,28 @@ pub mod proto {
 
 /// Initializes logger. Might do more things in the future.
 pub fn init() {
-  Logger::with_env_or_str("info")
-    .log_target(LogTarget::File)
-    .directory("log")
-    .duplicate_to_stdout(Duplicate::All)
-    .format(flexi_logger::opt_format)
-    .start()
+  // Put line numbers in debug builds, but not in release builds.
+  #[cfg(debug_assertions)]
+  let pat = PatternEncoder::new("{d(%Y-%m-%d %H:%M:%S:%f)} {f}:{L} [{h({l})}] {m}{n}");
+  #[cfg(not(debug_assertions))]
+  let pat = PatternEncoder::new("{d(%Y-%m-%d %H:%M:%S:%f)} [{h({l})}] {m}{n}");
+
+  let stdout = ConsoleAppender::builder().encoder(Box::new(pat.clone())).build();
+  let disk = FileAppender::builder().encoder(Box::new(pat)).build("log/latest.log").unwrap();
+
+  let config = Config::builder()
+    .appender(Appender::builder().build("stdout", Box::new(stdout)))
+    .appender(Appender::builder().build("disk", Box::new(disk)))
+    .build(Root::builder().appender("stdout").appender("disk").build(LevelFilter::Info))
     .unwrap();
+
+  log4rs::init_config(config).unwrap();
+
+  // Logger::with_env_or_str("info")
+  //   .log_target(LogTarget::File)
+  //   .directory("log")
+  //   .duplicate_to_stdout(Duplicate::All)
+  //   .format(flexi_logger::opt_format)
+  //   .start()
+  //   .unwrap();
 }
