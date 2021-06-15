526dd18953f66c386b266ef2eff9af9402d9b629
Neil Macneale <neil.macneale.v@gmail.com>
2021-06-15 03:38:44 -0700

    Can read an unconnected ping packet (this is a raknet packet, not a bedrock packet)

--- proxy/src/bedrock/stream.rs
+++ proxy/src/bedrock/stream.rs
@@ -1,14 +1,20 @@
 use crate::{packet::Packet, StreamReader, StreamWriter};
-use common::version::ProtocolVersion;
+use common::{util::Buffer, version::ProtocolVersion};
+use ringbuf::{Consumer, Producer, RingBuffer};
 use std::{
-  io,
+  io::{self, ErrorKind},
   net::SocketAddr,
   sync::{mpsc::Receiver, Arc},
 };
 use tokio::net::UdpSocket;
 
+const MAGIC: &'static [u8] =
+  &[0x00, 0xff, 0xff, 0x00, 0xfe, 0xfe, 0xfe, 0xfe, 0xfd, 0xfd, 0xfd, 0xfd, 0x12, 0x34, 0x56, 0x78];
+
 pub struct BedrockStreamReader {
-  rx: Receiver<Vec<u8>>,
+  rx:   Receiver<Vec<u8>>,
+  prod: Producer<u8>,
+  cons: Consumer<u8>,
 }
 
 pub struct BedrockStreamWriter {
@@ -18,7 +24,9 @@ pub struct BedrockStreamWriter {
 
 impl BedrockStreamReader {
   pub fn new(rx: Receiver<Vec<u8>>) -> Self {
-    BedrockStreamReader { rx }
+    let buf = RingBuffer::new(1024);
+    let (prod, cons) = buf.split();
+    BedrockStreamReader { rx, prod, cons }
   }
 }
 
@@ -37,12 +45,35 @@ impl StreamWriter for BedrockStreamWriter {
 #[async_trait]
 impl StreamReader for BedrockStreamReader {
   async fn poll(&mut self) -> io::Result<()> {
+    let msg = self.rx.recv().unwrap();
+    self.prod.push_slice(&msg);
     Ok(())
   }
   fn read(&mut self, ver: ProtocolVersion) -> io::Result<Option<Packet>> {
     info!("waiting for data...");
-    let data = self.rx.recv().unwrap();
-    info!("got data: {:?}", data);
+    let mut id = 0;
+    self.cons.access(|a, _| {
+      id = *a.get(0).unwrap_or(&0);
+    });
+    self.cons.discard(1);
+    match id {
+      // Unconnected Ping
+      1 | 2 => {
+        // Contains a long, MAGIC, and the client GUID
+        let mut data = vec![0; 8 + MAGIC.len() + 8];
+        self.cons.pop_slice(&mut data);
+        let mut buf = Buffer::new(data);
+
+        let time = buf.read_u64(); // time in millis
+        buf.expect(MAGIC);
+        let guid = buf.read_u64(); // client's guid
+        info!("got time/guid: {} {}", time, guid);
+      }
+      _ => {
+        return Err(io::Error::new(ErrorKind::InvalidData, format!("Unknown packet id: {}", id)))
+      }
+    }
+    info!("got packet id: {}", id);
     Ok(None)
   }
 }
