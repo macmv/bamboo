ed3f9522fad844686e9ddbb9ec68bfaf3356bc56
Neil Macneale <neil.macneale.v@gmail.com>
2021-06-26 16:02:01 -0700

    The prismarine data now uses fetch instead of clone to avoid cloning the entire repository

--- data/src/prismarine/mod.rs
+++ data/src/prismarine/mod.rs
@@ -1,16 +1,16 @@
 use std::{fs, io, path::Path, process::Command, str};
 
-fn checkout(path: &Path) {
-  // TODO: We want to fetch here if we can't check out.
-  let out = Command::new("git")
-    .current_dir(path)
-    .args(&["checkout", "140e1a64aa1f54d56d3e0a820e18e4432d6133ef"])
-    .output()
-    .expect("failed to execute git");
+const REMOTE: &str = "https://github.com/PrismarineJS/minecraft-data.git";
+const COMMIT: &str = "140e1a64aa1f54d56d3e0a820e18e4432d6133ef";
+
+fn run_git(path: &Path, args: &[&str]) -> String {
+  let out =
+    Command::new("git").current_dir(path).args(args).output().expect("failed to execute git");
   dbg!(&out);
+  let stdout = str::from_utf8(&out.stdout).unwrap();
+  println!("{}", stdout);
   assert_eq!(0, out.status.code().unwrap()); // Make sure this didn't fail
-  let out = str::from_utf8(&out.stdout).unwrap();
-  println!("{}", out);
+  return stdout.into();
 }
 
 pub fn clone(dir: &Path) -> io::Result<()> {
@@ -19,15 +19,7 @@ pub fn clone(dir: &Path) -> io::Result<()> {
   if !path.exists() {
     fs::create_dir_all(&path)?;
   }
-  let out = Command::new("git")
-    .current_dir(&path)
-    .args(&["rev-parse", "--show-toplevel"])
-    .output()
-    .expect("failed to execute git");
-  dbg!(&out);
-  assert_eq!(0, out.status.code().unwrap()); // Make sure this dodn't fail
-  let out = str::from_utf8(&out.stdout).unwrap();
-  println!("top level: {}", out);
+  let out = run_git(&path, &["rev-parse", "--show-toplevel"]);
   if !out.ends_with("prismarine-data") {
     // Means that the root repo is not prismarine data, so we need to clone
     if fs::read_dir(&path)?.next().is_some() {
@@ -35,19 +27,10 @@ pub fn clone(dir: &Path) -> io::Result<()> {
       fs::create_dir_all(&path)?;
     }
     println!("cloning repository...");
-    let out = Command::new("git")
-      .current_dir(&path)
-      .args(&["clone", "https://github.com/PrismarineJS/minecraft-data.git", "."])
-      .output()
-      .expect("failed to execute git");
-    dbg!(&out);
-    assert_eq!(0, out.status.code().unwrap()); // Make sure this dodn't fail
-    let out = str::from_utf8(&out.stdout).unwrap();
-    println!("{}", out);
-    checkout(&path);
-  } else {
-    // We already have a good repo, make sure we are checked out correctly
-    checkout(&path);
+    run_git(&path, &["init"]);
+    run_git(&path, &["remote", "add", "origin", REMOTE]);
+    run_git(&path, &["fetch", "origin", COMMIT, "--depth=1"]);
   }
+  run_git(&path, &["checkout", COMMIT]);
   Ok(())
 }
