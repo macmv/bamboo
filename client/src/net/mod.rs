d615a2f1d5864bfb0166e4b327753398e02531d5
Neil Macneale <neil.macneale.v@gmail.com>
2021-06-27 14:55:20 -0700

    Setup the handshake function to send a handshake, although packet writing is not yet implemented

--- client/src/net/mod.rs
+++ client/src/net/mod.rs
@@ -1,3 +1,5 @@
+use common::{net::tcp, version::ProtocolVersion};
+use std::net::IpAddr;
 use tokio::net::TcpStream;
 
 mod reader;
@@ -24,10 +26,21 @@ impl Connection {
 
     let (read, write) = stream.into_split();
     let conn = Connection { reader: TCPReader::new(read), writer: TCPWriter::new(write) };
-    conn.handshake("macmv");
+    conn.handshake(ip, "macmv").await;
 
     Some(conn)
   }
 
-  async fn handshake(&self, name: &str) {}
+  async fn handshake(&self, ip: &str, name: &str) {
+    let out = tcp::Packet::new(0, ProtocolVersion::V1_8); // Handshake
+    out.write_varint(ProtocolVersion::V1_8.id() as i32); // Protocol version
+    out.write_str(ip); // Ip
+    out.write_u16(25565); // Port
+    out.write_varint(2); // Going to login
+    self.writer.send(out).await;
+
+    let out = tcp::Packet::new(0, ProtocolVersion::V1_8); // Login start
+    out.write_str(name); // Username
+    self.writer.send(out).await;
+  }
 }
