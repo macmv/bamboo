0b44f014980584100e7cbda479991ea1fdd632de
Neil Macneale <neil.macneale.v@gmail.com>
2021-06-27 22:19:53 -0700

    Can decode a DER key (tested with a vanilla server and custom client) but cannot serialize a bitstring

--- common/src/math/der.rs
+++ common/src/math/der.rs
@@ -4,16 +4,25 @@ use rsa::{PaddingScheme, PublicKey, PublicKeyParts, RSAPrivateKey, RSAPublicKey}
 pub fn decode(bytes: &[u8]) -> Option<RSAPublicKey> {
   let result: asn1::ParseResult<_> = asn1::parse(bytes, |d| {
     return d.read_element::<asn1::Sequence>()?.parse(|d| {
-      let alg = d
-        .read_element::<asn1::Sequence>()?
-        .parse(|d| Ok(d.read_element::<asn1::ObjectIdentifier>()?))?;
-      info!("{}", alg);
-      let pub_key = d.read_element::<asn1::BitString>()?;
-      let (n, e) = asn1::parse(pub_key.as_bytes(), |d| {
-        let n = d.read_element::<BigUint>()?;
-        let e = d.read_element::<BigUint>()?;
-        Ok((n, e))
+      d.read_element::<asn1::Sequence>()?.parse(|d| {
+        let alg = d.read_element::<asn1::ObjectIdentifier>()?;
+        let params = d.read_element::<asn1::Tlv>()?;
+        info!("alg: {}", alg);
+        info!("params: {:?}", params);
+        Ok(())
       })?;
+      let pub_key = d.read_element::<asn1::BitString>()?;
+      info!("parsed key");
+      let (n, e) = asn1::parse(pub_key.as_bytes(), |d| {
+        d.read_element::<asn1::Sequence>()?.parse(|d| {
+          info!("reading n...");
+          let n = d.read_element::<BigUint>()?;
+          info!("reading e...");
+          let e = d.read_element::<BigUint>()?;
+          Ok((n, e))
+        })
+      })?;
+      info!("got n, e: {:?} {:?}", n, e);
       Ok((n, e))
     });
   });
@@ -26,14 +35,27 @@ pub fn decode(bytes: &[u8]) -> Option<RSAPublicKey> {
 pub fn encode(key: &RSAPublicKey) -> Vec<u8> {
   asn1::write(|w| {
     w.write_element(&asn1::SequenceWriter::new(&|w| {
-      // asn1 BigUint requires the first byte to be a 0, to disambiguate from negative
-      // values
-      let mut n = vec![0];
-      n.append(&mut key.n().to_bytes_be());
-      w.write_element(&BigUint::new(&n).unwrap());
-      let mut e = vec![0];
-      e.append(&mut key.e().to_bytes_be());
-      w.write_element(&BigUint::new(&n).unwrap());
+      // A sequence containing the algorithm used.
+      w.write_element(&asn1::SequenceWriter::new(&|w| {
+        w.write_element(&asn1::ObjectIdentifier::from_string("1.2.840.113549.1.1.1"));
+        w.write_element(&()); // NULL value
+      }));
+      // A bitstring containing the N and E of the key
+      w.write_element(
+        &asn1::write(|w| {
+          w.write_element(&asn1::SequenceWriter::new(&|w| {
+            // asn1 BigUint requires the first byte to be a 0, to disambiguate from negative
+            // values
+            let mut n = vec![0];
+            n.append(&mut key.n().to_bytes_be());
+            w.write_element(&BigUint::new(&n).unwrap());
+            let mut e = vec![0];
+            e.append(&mut key.e().to_bytes_be());
+            w.write_element(&BigUint::new(&n).unwrap());
+          }));
+        })
+        .as_slice(),
+      );
     }));
   })
 }
