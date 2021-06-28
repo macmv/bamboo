a03765d117626f46fb53c13f21213b6468392cc6
Neil Macneale <neil.macneale.v@gmail.com>
2021-06-27 22:54:27 -0700

    DER encoding is working correcly now

--- common/src/math/der.rs
+++ common/src/math/der.rs
@@ -1,6 +1,35 @@
 use asn1::BigUint;
 use rsa::{PaddingScheme, PublicKey, PublicKeyParts, RSAPrivateKey, RSAPublicKey};
 
+/// Represents an ASN.1 `BIT STRING`. Need this because the constructor is
+/// private in the asn1 crate.
+#[derive(Debug, PartialEq, Clone)]
+pub struct BitString<'a> {
+  data:    &'a [u8],
+  padding: u8,
+}
+
+impl<'a> BitString<'a> {
+  pub fn new(data: &'a [u8], padding: u8) -> Option<BitString<'a>> {
+    if padding > 7 || (data.is_empty() && padding != 0) {
+      return None;
+    }
+    if padding > 0 && data[data.len() - 1] & ((1 << padding) - 1) != 0 {
+      return None;
+    }
+
+    Some(BitString { data, padding })
+  }
+}
+
+impl<'a> asn1::SimpleAsn1Writable<'a> for BitString<'a> {
+  const TAG: u8 = 0x03;
+  fn write_data(&self, dest: &mut Vec<u8>) {
+    dest.push(self.padding);
+    dest.extend_from_slice(self.data);
+  }
+}
+
 pub fn decode(bytes: &[u8]) -> Option<RSAPublicKey> {
   let result: asn1::ParseResult<_> = asn1::parse(bytes, |d| {
     return d.read_element::<asn1::Sequence>()?.parse(|d| {
@@ -27,9 +56,31 @@ pub fn decode(bytes: &[u8]) -> Option<RSAPublicKey> {
     });
   });
 
-  dbg!(result);
+  let (n, e) = result.unwrap();
 
-  None
+  Some(
+    RSAPublicKey::new(
+      rsa::BigUint::from_bytes_be(n.as_bytes()),
+      rsa::BigUint::from_bytes_be(e.as_bytes()),
+    )
+    .unwrap(),
+  )
+}
+
+fn write_big_uint(w: &mut asn1::Writer, int: &rsa::BigUint) {
+  let mut bytes = int.to_bytes_be();
+  let out;
+  let mut tmp = vec![];
+  // asn1 BigUint requires the first byte to be a 0, to disambiguate from negative
+  // values
+  if bytes[0] & 0x80 != 0 {
+    tmp.push(0);
+    tmp.append(&mut bytes);
+    out = BigUint::new(tmp.as_ref()).unwrap()
+  } else {
+    out = BigUint::new(&bytes).unwrap()
+  }
+  w.write_element(&out);
 }
 
 pub fn encode(key: &RSAPublicKey) -> Vec<u8> {
@@ -42,19 +93,16 @@ pub fn encode(key: &RSAPublicKey) -> Vec<u8> {
       }));
       // A bitstring containing the N and E of the key
       w.write_element(
-        &asn1::write(|w| {
-          w.write_element(&asn1::SequenceWriter::new(&|w| {
-            // asn1 BigUint requires the first byte to be a 0, to disambiguate from negative
-            // values
-            let mut n = vec![0];
-            n.append(&mut key.n().to_bytes_be());
-            w.write_element(&BigUint::new(&n).unwrap());
-            let mut e = vec![0];
-            e.append(&mut key.e().to_bytes_be());
-            w.write_element(&BigUint::new(&n).unwrap());
-          }));
-        })
-        .as_slice(),
+        &BitString::new(
+          &asn1::write(|w| {
+            w.write_element(&asn1::SequenceWriter::new(&|w| {
+              write_big_uint(w, key.n());
+              write_big_uint(w, key.e());
+            }));
+          }),
+          0,
+        )
+        .unwrap(),
       );
     }));
   })
