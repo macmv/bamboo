2c8fa4b024a54e7308b0a4d05423267451251f8a
Neil Macneale <neil.macneale.v@gmail.com>
2021-06-27 19:33:29 -0700

    Client has a small loop that allows it to finish an un-encrypted handshake

--- client/src/net/mod.rs
+++ client/src/net/mod.rs
@@ -2,16 +2,27 @@ use common::{
   net::tcp,
   stream::{
     java::{self, JavaStreamReader, JavaStreamWriter},
-    StreamWriter,
+    StreamReader, StreamWriter,
   },
   version::ProtocolVersion,
 };
-use std::io;
+use std::{io, io::ErrorKind};
 use tokio::net::TcpStream;
+
+#[derive(Debug, Copy, Clone)]
+pub enum State {
+  Handshake,
+  Status,
+  Login,
+  Play,
+  Invalid,
+}
 
 pub struct Connection {
   reader: JavaStreamReader,
   writer: JavaStreamWriter,
+  ver:    ProtocolVersion,
+  state:  State,
 }
 
 impl Connection {
@@ -26,7 +37,8 @@ impl Connection {
     };
 
     let (reader, writer) = java::stream::new(tcp_stream).unwrap();
-    let mut conn = Connection { reader, writer };
+    let mut conn =
+      Connection { reader, writer, ver: ProtocolVersion::V1_8, state: State::Handshake };
     if let Err(e) = conn.handshake(ip, "macmv").await {
       error!("could not finish handshake with {}: {}", ip, e);
       return None;
@@ -36,16 +48,193 @@ impl Connection {
   }
 
   async fn handshake(&mut self, ip: &str, name: &str) -> Result<(), io::Error> {
-    let mut out = tcp::Packet::new(0, ProtocolVersion::V1_8); // Handshake
-    out.write_varint(ProtocolVersion::V1_8.id() as i32); // Protocol version
+    let mut out = tcp::Packet::new(0, self.ver); // Handshake
+    out.write_varint(self.ver.id() as i32); // Protocol version
     out.write_str(ip); // Ip
     out.write_u16(25565); // Port
     out.write_varint(2); // Going to login
     self.writer.write(out).await?;
+    self.state = State::Login;
 
-    let mut out = tcp::Packet::new(0, ProtocolVersion::V1_8); // Login start
+    let mut out = tcp::Packet::new(0, self.ver); // Login start
     out.write_str(name); // Username
     self.writer.write(out).await?;
+
+    'login: loop {
+      self.reader.poll().await.unwrap();
+      loop {
+        let p = self.reader.read(self.ver).unwrap();
+        if p.is_none() {
+          break;
+        }
+        let mut p = p.unwrap();
+        let err = p.err();
+        match err {
+          Some(e) => {
+            error!("error while parsing packet: {}", e);
+            break;
+          }
+          None => {}
+        }
+        info!("got packet id: {}", p.id());
+        match self.state {
+          State::Handshake => unreachable!(),
+          State::Status => {
+            info!("got status packet: {}", p.id());
+          }
+          State::Login => {
+            match p.id() {
+              // Disconnect
+              0 => {
+                info!("got disconnect packet from server during login");
+                return Ok(());
+              }
+              // Encryption request
+              1 => {
+                let _server_id = p.read_str();
+                let key_len = p.read_varint();
+                let der_key = p.read_buf(key_len);
+                let token_len = p.read_varint();
+                let token = p.read_buf(token_len);
+
+                let pub_key = rsa_der::public_key_from_der(&der_key);
+
+                // if username.is_some() {
+                //   return Err(io::Error::new(
+                //     ErrorKind::InvalidInput,
+                //     "client sent two login packets",
+                //   ));
+                // }
+                // let name = p.read_str();
+                // username = Some(name.to_string());
+                // if der_key.is_none() {
+                //   info = Some(LoginInfo {
+                //     // Generate uuid if we are in offline mode
+                //     id: UUID::from_bytes(*md5::compute(&name)),
+                //     name,
+                //     properties: vec![],
+                //   });
+                // }
+                //
+                // match &der_key {
+                //   Some(key) => {
+                //     // Make sure to actually generate a token
+                //     OsRng.fill_bytes(&mut token);
+                //
+                //     // Encryption request
+                //     let mut out = tcp::Packet::new(1, self.ver);
+                //     out.write_str(""); // Server id, should be empty
+                //     out.write_varint(key.len() as i32); // Key len
+                //     out.write_buf(key); // DER encoded RSA key
+                //     out.write_varint(4); // Token len
+                //     out.write_buf(&token); // Verify token
+                //     self.writer.write(out).await?;
+                //     // Wait for encryption response to enable encryption
+                //   }
+                //   None => {
+                //     self.send_compression(compression).await?;
+                //     self.send_success(info.as_ref().unwrap()).await?;
+                //     // Successful login, we can break now
+                //     break 'login;
+                //   }
+                // }
+              }
+              // Login success
+              2 => {
+                // if username.is_none() {
+                //   return Err(io::Error::new(
+                //     ErrorKind::InvalidInput,
+                //     "client did not send login start before sending ecryption response",
+                //   ));
+                // }
+                // let len = p.read_varint();
+                // let recieved_secret = p.read_buf(len);
+                // let len = p.read_varint();
+                // let recieved_token = p.read_buf(len);
+                //
+                // let decrypted_secret =
+                //   key.decrypt(PaddingScheme::PKCS1v15Encrypt, &recieved_secret).unwrap();
+                // let decrypted_token =
+                //   key.decrypt(PaddingScheme::PKCS1v15Encrypt, &recieved_token).unwrap();
+                //
+                // // Make sure the client sent the correct verify token back
+                // if decrypted_token != token {
+                //   return Err(io::Error::new(
+                //     ErrorKind::InvalidInput,
+                //     format!(
+                //       "invalid verify token recieved from client (len: {})",
+                //       decrypted_token.len()
+                //     ),
+                //   ));
+                // }
+                // let len = decrypted_secret.len();
+                // let secret = match decrypted_secret.try_into() {
+                //   Ok(v) => v,
+                //   Err(_) => {
+                //     return Err(io::Error::new(
+                //       ErrorKind::InvalidInput,
+                //       format!(
+                //         "invalid secret recieved from client (len: {}, expected len 16)",
+                //         len,
+                //       ),
+                //     ))
+                //   }
+                // };
+                //
+                // let mut hash = Sha1::new();
+                // hash.update("");
+                // hash.update(secret);
+                // hash.update(der_key.unwrap());
+                // info = match reqwest::get(format!(
+                //   "https://sessionserver.mojang.com/session/minecraft/hasJoined?username={}&serverId={}",
+                //   username.as_ref().unwrap(),
+                //   hexdigest(hash)
+                // )).await {
+                //   Ok(v) => match v.json().await {
+                //     Ok(v) => Some(v),
+                //     Err(e) => return Err(io::Error::new(
+                //       ErrorKind::InvalidData,
+                //       format!("invalid json data recieved from session server: {}", e),
+                //     ))
+                //   },
+                //   Err(e) => return Err(io::Error::new(
+                //     ErrorKind::Other,
+                //     format!("failed to authenticate client: {}", e),
+                //   ))
+                // };
+                //
+                // self.writer.enable_encryption(&secret);
+                // self.reader.enable_encryption(&secret);
+                //
+                // self.send_compression(compression).await?;
+                // self.send_success(info.as_ref().unwrap()).await?;
+                // Successful login, we can break now
+                info!("successful login");
+                break 'login;
+              }
+              // Set compression
+              3 => {
+                let level = p.read_varint();
+                self.reader.set_compression(level);
+                self.writer.set_compression(level);
+              }
+              _ => {
+                return Err(io::Error::new(
+                  ErrorKind::InvalidInput,
+                  format!("unknown login packet {}", p.id()),
+                ));
+              }
+            }
+          }
+          v => {
+            return Err(io::Error::new(
+              ErrorKind::InvalidInput,
+              format!("invalid connection state {:?}", v),
+            ));
+          }
+        }
+      }
+    }
 
     Ok(())
   }
