884f2b0d4095f4c4582e8b566cf464d86d6cecea
Neil Macneale <neil.macneale.v@gmail.com>
2021-06-28 21:06:52 -0700

    Added an from_proto function to MeshChunk, then realized that you cannot create a chunk from a protobuf

--- client/src/graphics/chunk.rs
+++ client/src/graphics/chunk.rs
@@ -1,4 +1,4 @@
-use common::chunk::Chunk;
+use common::{chunk::Chunk, proto};
 
 /// A chunk with a mesh. This acts the same as a normal mesh, but will lazily
 /// update a mesh any time it needs to be rendered.
@@ -10,4 +10,6 @@ impl MeshChunk {
   pub fn new(chunk: Chunk) -> Self {
     MeshChunk { chunk }
   }
+
+  pub fn from_proto(p: proto::Chunk) -> Self {}
 }
