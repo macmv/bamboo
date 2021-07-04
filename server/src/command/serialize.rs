981a2ab9f3b65e191c5c940f19c0e2463d227ba8
Neil Macneale <neil.macneale.v@gmail.com>
2021-07-04 16:07:25 -0700

    Added a name() function for command parsers

--- server/src/command/serialize.rs
+++ server/src/command/serialize.rs
@@ -1,4 +1,4 @@
-use super::{Command, CommandTree, NodeType};
+use super::{Command, CommandTree, NodeType, Parser};
 use common::{net::cb, util::Buffer};
 
 impl NodeType {
@@ -72,5 +72,61 @@ impl Command {
     let children = self.children.iter().map(|c| c.write_nodes(nodes)).collect();
     nodes.push(IndexNode { name: self.name.clone(), ty: self.ty.clone(), children });
     nodes.len() - 1
+  }
+}
+
+impl Parser {
+  /// Returns the name of this parser. Used in packet serialization.
+  #[rustfmt::skip]
+  pub fn name(&self) -> &'static str {
+    match self {
+      Self::Bool               => "minecraft:bool",
+      Self::Double { .. }      => "minecraft:double",
+      Self::Float { .. }       => "minecraft:float",
+      Self::Int { .. }         => "minecraft:int",
+      Self::String(_)          => "minecraft:string",
+      Self::Entity { .. }      => "minecraft:entity",
+      Self::ScoreHolder { .. } => "minecraft:score_holder",
+      Self::GameProfile        => "minecraft:game_profile",
+      Self::BlockPos           => "minecraft:block_pos",
+      Self::ColumnPos          => "minecraft:column_pos",
+      Self::Vec3               => "minecraft:vec3",
+      Self::Vec2               => "minecraft:vec2",
+      Self::BlockState         => "minecraft:block_state",
+      Self::BlockPredicate     => "minecraft:block_predicate",
+      Self::ItemStack          => "minecraft:item_stack",
+      Self::ItemPredicate      => "minecraft:item_predicate",
+      Self::Color              => "minecraft:color",
+      Self::Component          => "minecraft:component",
+      Self::Message            => "minecraft:message",
+      Self::Nbt                => "minecraft:nbt",
+      Self::NbtPath            => "minecraft:nbt_path",
+      Self::Objective          => "minecraft:objective",
+      Self::ObjectiveCriteria  => "minecraft:objective_criteria",
+      Self::Operation          => "minecraft:operation",
+      Self::Particle           => "minecraft:particle",
+      Self::Rotation           => "minecraft:rotation",
+      Self::Angle              => "minecraft:angle",
+      Self::ScoreboardSlot     => "minecraft:scoreboard_slot",
+      Self::Swizzle            => "minecraft:swizzle",
+      Self::Team               => "minecraft:team",
+      Self::ItemSlot           => "minecraft:item_slot",
+      Self::ResourceLocation   => "minecraft:resource_location",
+      Self::MobEffect          => "minecraft:mob_effect",
+      Self::Function           => "minecraft:function",
+      Self::EntityAnchor       => "minecraft:entity_anchor",
+      Self::Range { .. }       => "minecraft:range",
+      Self::IntRange           => "minecraft:int_range",
+      Self::FloatRange         => "minecraft:float_range",
+      Self::ItemEnchantment    => "minecraft:item_enchantment",
+      Self::EntitySummon       => "minecraft:entity_summon",
+      Self::Dimension          => "minecraft:dimension",
+      Self::Uuid               => "minecraft:uuid",
+      Self::NbtTag             => "minecraft:nbt_tag",
+      Self::NbtCompoundTag     => "minecraft:nbt_compound_tag",
+      Self::Time               => "minecraft:time",
+      Self::Modid              => "forge:modid",
+      Self::Enum               => "forge:enum",
+    }
   }
 }
