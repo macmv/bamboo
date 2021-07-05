3e4eb23301a4d99baee6ef55fbea336239b9aad9
Neil Macneale <neil.macneale.v@gmail.com>
2021-07-05 12:09:15 -0700

    Added a /fill command, which the client parses correctly

--- server/src/world/init.rs
+++ server/src/world/init.rs
@@ -8,6 +8,17 @@ impl World {
     c.add_arg("text", Parser::String(StringType::Greedy));
     self.get_commands().add(c);
 
+    let mut c = Command::new("fill");
+    c.add_lit("rect")
+      .add_arg("min", Parser::BlockPos)
+      .add_arg("max", Parser::BlockPos)
+      .add_arg("block", Parser::BlockState);
+    c.add_lit("circle")
+      .add_arg("center", Parser::BlockPos)
+      .add_arg("radius", Parser::Float { min: Some(0.0), max: None })
+      .add_arg("block", Parser::BlockState);
+    self.get_commands().add(c);
+
     info!("generating terrain...");
     for x in -10..=10 {
       for z in -10..=10 {
