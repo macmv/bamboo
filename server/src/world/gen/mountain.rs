fa850c2fff720e7276dc99d24d40444bbc91c6d5
Neil Macneale <neil.macneale.v@gmail.com>
2021-08-16 15:25:25 -0700

    Mountains ended up looking like a plateu type of thing

--- server/src/world/gen/mountain.rs
+++ server/src/world/gen/mountain.rs
@@ -1,6 +1,7 @@
 use super::{BiomeGen, BiomeLayers, WorldGen};
 use crate::block;
 use common::math::Pos;
+use std::cmp;
 
 pub struct Gen {
   id: usize,
@@ -18,6 +19,13 @@ impl BiomeGen for Gen {
   }
   fn height_at(&self, world: &WorldGen, pos: Pos) -> i32 {
     let dist = world.dist_to_border(pos);
-    world.height_at(pos) as i32 + (dist * 0.5) as i32
+    let mut height = world.height_at(pos) as i32;
+    if dist > 12.0 {
+      height += (10.0_f64.powi(2) / 10.0) as i32;
+      height += ((dist - 12.0).sqrt()) as i32;
+    } else if dist > 2.0 {
+      height += ((dist - 2.0).powi(2) / 10.0) as i32;
+    }
+    cmp::min(height, 255)
   }
 }
