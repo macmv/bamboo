f7693e074ae628818aca98f8ca6cad59349a8e90
Neil Macneale <neil.macneale.v@gmail.com>
2021-08-25 05:49:43 -0700

    Can join on 1.13

--- server/src/net/serialize/mod.rs
+++ server/src/net/serialize/mod.rs
@@ -1,6 +1,7 @@
 use crate::world::chunk::MultiChunk;
 use common::{math::ChunkPos, net::cb, version::BlockVersion};
 
+mod v1_13;
 mod v1_8;
 mod v1_9;
 
@@ -8,6 +9,7 @@ pub fn serialize_chunk(pos: ChunkPos, c: &MultiChunk, ver: BlockVersion) -> cb::
   match ver {
     BlockVersion::V1_8 => v1_8::serialize_chunk(pos, c),
     BlockVersion::V1_9 | BlockVersion::V1_12 => v1_9::serialize_chunk(pos, c, ver),
+    BlockVersion::V1_13 => v1_13::serialize_chunk(pos, c),
     ver => unimplemented!("cannot serialize chunks for version {:?}", ver),
   }
 }
