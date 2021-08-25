bc7b34fff53f48fad285db8054b29b191ce6d768
Neil Macneale <neil.macneale.v@gmail.com>
2021-08-24 18:26:25 -0700

    1.8 chunk are serialized correctly, but there is still a problem with extra fields being added into 1.8 packets

--- server/src/net/serialize/v1_8.rs
+++ server/src/net/serialize/v1_8.rs
@@ -1,8 +1,8 @@
 use crate::world::chunk::MultiChunk;
 use common::{math::ChunkPos, net::cb, util::Buffer};
+use std::convert::TryInto;
 
 pub fn serialize_chunk(pos: ChunkPos, c: &MultiChunk) -> cb::Packet {
-  let mut chunk_data = Buffer::new(vec![]);
   let mut bit_map = 0;
   let c = c.get_fixed();
 
@@ -10,15 +10,29 @@ pub fn serialize_chunk(pos: ChunkPos, c: &MultiChunk) -> cb::Packet {
   let biomes = true;
 
   let mut total_sections = 0;
-  // Flatten removes all the None chunks
+  for s in c.sections().into_iter() {
+    if s.is_some() {
+      total_sections += 1;
+    }
+  }
+
+  let data_len = total_sections * 16 * 16 * 16 * 2 // Chunk data
+    + (total_sections * 16 * 16 * 16 / 2) // Block light
+    + if skylight { total_sections * 16 * 16 * 16 / 2 } else { 0 } // Sky light
+    + if biomes { 256 } else { 0 }; // Biomes
+
+  // The most it will be is data_len + max varint len
+  let mut chunk_data = Buffer::new(Vec::with_capacity(data_len + 5));
+  chunk_data.write_varint(data_len.try_into().unwrap());
+  let prefix_len = chunk_data.len();
+
   for (y, s) in c.sections().into_iter().enumerate() {
     if let Some(s) = s {
       let s = s.unwrap_fixed();
       bit_map |= 1 << y;
-      total_sections += 1;
-      // These are little endian. I don't know why. It probably has something to do
-      // with the way I serialize things, but I couldn't really be bothered to figure
-      // it out (because it works).
+      // These are little endian. I'm assuming that the person who wrote the
+      // networking for Minecraft and the person who wrote chunk decoding were not
+      // consistent.
       chunk_data
         .write_buf(&s.data().iter().map(|v| v.to_le_bytes()).flatten().collect::<Vec<u8>>());
     }
@@ -39,6 +53,8 @@ pub fn serialize_chunk(pos: ChunkPos, c: &MultiChunk) -> cb::Packet {
       chunk_data.write_u8(127); // Void biome
     }
   }
+  debug_assert_eq!(chunk_data.len() - prefix_len, data_len, "unexpected chunk data len");
+
   cb::Packet::MapChunk {
     x:                     pos.x(),
     z:                     pos.z(),
