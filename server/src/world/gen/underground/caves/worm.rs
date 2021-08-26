223556a7f1774f8e77bc9dc999d9ad2f8ade9696
Neil Macneale <neil.macneale.v@gmail.com>
2021-08-25 17:33:13 -0700

    The worms go downwards as expected, but do not branch yet

--- server/src/world/gen/underground/caves/worm.rs
+++ server/src/world/gen/underground/caves/worm.rs
@@ -16,7 +16,7 @@ pub struct CaveWorm {
 impl CaveWorm {
   pub fn new(seed: u64, pos: Pos) -> Self {
     let mut rng = WyhashRng::new(seed);
-    let angle_y = ((rng.next_u32() % 1000) as f64 / 1000.0 - 0.5) * std::f64::consts::PI; // offset by +- PI
+    let angle_y = ((rng.next_u32() % 1000) as f64 / 1000.0 - 0.5) * std::f64::consts::PI; // -PI to PI
     let mut direction = Vec3 { x: 0.0, y: 0.0, z: 0.0 };
     direction.x = angle_y.cos();
     direction.z = angle_y.sin();
@@ -26,7 +26,7 @@ impl CaveWorm {
     let steps = self.rng.next_u32() % 100 + 20;
     for _ in 0..steps {
       if self.pos.chunk() == chunk_pos {
-        c.set_kind(self.pos.chunk_rel(), block::Kind::GreenWool).unwrap();
+        c.set_kind(self.pos.chunk_rel(), block::Kind::LimeWool).unwrap();
       }
       self.advance();
     }
@@ -44,9 +44,14 @@ impl CaveWorm {
     if self.pos.y() < 0 {
       self.pos = self.pos.with_y(0);
     }
+    let angle_vert = ((self.rng.next_u32() % 1024) as f64 / 1024.0) * 0.6 - 0.5; // -0.5 to 0.1
+    self.direction.y = angle_vert.sin();
+
     let mut angle_y = (self.direction.z / self.direction.x).atan();
-    angle_y += ((self.rng.next_u32() % 1000) as f64 / 1000.0 - 0.5) * 0.8; // offset by +- 0.8
-    self.direction.x = angle_y.cos();
-    self.direction.z = angle_y.sin();
+    angle_y += ((self.rng.next_u32() % 1024) as f64 / 1024.0 - 0.5) * 0.8; // -0.8 to 0.8
+
+    // Need to divide, to keep the vector normalized
+    self.direction.x = angle_y.cos() / angle_vert.cos();
+    self.direction.z = angle_y.sin() / angle_vert.cos();
   }
 }
