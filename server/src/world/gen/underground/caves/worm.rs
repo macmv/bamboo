726e6ec8fd402f6d65a1b22cc76e17481e7c1f32
Neil Macneale <neil.macneale.v@gmail.com>
2021-09-08 01:27:51 -0700

    Cave worms no longer store their children, instead they store the positions their children generate

--- server/src/world/gen/underground/caves/worm.rs
+++ server/src/world/gen/underground/caves/worm.rs
@@ -16,15 +16,13 @@ pub struct CaveWorm {
   // direction: Vec3,
   angle_vert: f64,
   angle_horz: f64,
-
-  children: Vec<CaveWorm>,
 }
 
 impl CaveWorm {
   pub fn new(seed: u64, pos: Pos) -> Self {
     let mut rng = WyhashRng::new(seed);
     let angle_horz = ((rng.next_u32() % 1000) as f64 / 1000.0 - 0.5) * std::f64::consts::PI; // -PI to PI
-    CaveWorm { rng, pos, steps: vec![], angle_vert: 0.0, angle_horz, children: vec![] }
+    CaveWorm { rng, pos, steps: vec![], angle_vert: 0.0, angle_horz }
   }
   pub fn process(&self, chunk_pos: ChunkPos, c: &mut MultiChunk) {
     for pos in &self.steps {
@@ -35,13 +33,11 @@ impl CaveWorm {
         c.fill_kind(min, max, block::Kind::Air).unwrap();
       }
     }
-    for child in &self.children {
-      child.process(chunk_pos, c);
-    }
   }
 
-  /// Generates a cave path. This will recursivly add children, which will carve
-  /// shorter paths.
+  /// Generates a cave path. This will recursivly spawn children, which will
+  /// carve shorter paths. All children's paths will be appended to
+  /// `self.steps`.
   pub fn carve(&mut self, offset: u32) {
     let steps = self.rng.next_u32() % 10 + 20;
     if steps < offset {
@@ -53,7 +49,7 @@ impl CaveWorm {
       if self.rng.next_u32() % 16 == 0 {
         let mut worm = CaveWorm::new(self.rng.next_u64(), self.pos);
         worm.carve(step + 5);
-        self.children.push(worm);
+        self.steps.append(&mut worm.steps);
       }
     }
   }
