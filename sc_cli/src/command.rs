3fafbb78795fdf864bbc02e01f7650671bcf583b
Neil Macneale <neil.macneale.v@gmail.com>
2021-09-24 20:58:41 -0700

    The say command and any command that starts with / will be sent as chat messages

--- sc_cli/src/command.rs
+++ sc_cli/src/command.rs
@@ -1,5 +1,6 @@
 use super::ConnWriter;
 use crate::cli::LineReader;
+use sc_common::net::sb;
 use std::{io, io::Write};
 
 pub async fn handle(
@@ -11,6 +12,19 @@ pub async fn handle(
   match command {
     "send" => {
       writeln!(l, "running send")?;
+    }
+    "say" => {
+      let saying = args.join(" ");
+      writeln!(l, "saying {}", saying)?;
+      w.write(sb::Packet::Chat { message: saying.into() }).await?;
+      w.flush().await?;
+    }
+    c if c.starts_with('/') => {
+      let mut out = command.to_string();
+      out.push_str(&args.join(" "));
+      writeln!(l, "sending command {}", out)?;
+      w.write(sb::Packet::Chat { message: out }).await?;
+      w.flush().await?;
     }
     _ => {
       writeln!(l, "unknown command: {}", command)?;
