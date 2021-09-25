7e7ccf4144d17737cf8fece48beae634ebfee05f
Neil Macneale <neil.macneale.v@gmail.com>
2021-09-24 20:17:24 -0700

    Can parse some escape codes

--- sc_cli/src/cli.rs
+++ sc_cli/src/cli.rs
@@ -154,6 +154,8 @@ impl LineReader {
 
     let mut out = String::new();
     let mut reader = io::stdin();
+    let mut in_escape = false;
+    let mut escape = String::new();
     loop {
       let mut buf = [0; 1];
       reader.read(&mut buf)?;
@@ -178,14 +180,47 @@ impl LineReader {
           }
           continue;
         }
+        b'\x1b' => {
+          // escape (things like arrows keys)
+          in_escape = true;
+          continue;
+        }
         _ => {}
       }
-      out.push(c as char);
+      if in_escape {
+        escape.push(c as char);
+        in_escape = self.parse_escape(&escape)?;
+        if !in_escape {
+          escape.clear();
+        }
+      } else {
+        out.push(c as char);
 
-      self.buf.write(&[c])?;
-      self.buf.flush()?;
+        self.buf.write(&[c])?;
+        self.buf.flush()?;
+      }
     }
     Ok(out)
+  }
+
+  fn parse_escape(&self, code: &str) -> io::Result<bool> {
+    let bytes = code.as_bytes();
+    // incomplete
+    if bytes.len() < 2 {
+      return Ok(true);
+    }
+    if bytes[0] != b'[' {
+      return Ok(false);
+    }
+    match bytes[1] {
+      b'A' => io::stdout().write(b"a")?,       // up
+      b'B' => io::stdout().write(b"b")?,       // down
+      b'C' => io::stdout().write(b"\x1b[1C")?, // left
+      b'D' => io::stdout().write(b"\x1b[1D")?, // right
+      _ => 0,
+    };
+    io::stdout().flush()?;
+    Ok(false)
   }
 }
 
