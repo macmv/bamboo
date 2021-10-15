945341b3e8fb252132629089c6c1ef398727850e
Neil Macneale <neil.macneale.v@gmail.com>
2021-10-15 16:03:40 -0700

    The server binary compiles. Nothing works.
    Some things work. Terrain generates on another thread, and we can listen
    for clients. However, the only reason that works is because we haven't
    run out of tokio threads. That terrain generation is a blocking thread
    running in a tokio task. Needless to say, this needs a lot of fixing.

--- sc_server/src/main.rs
+++ sc_server/src/main.rs
@@ -1,58 +1,51 @@
 #[macro_use]
 extern crate log;
 
-use std::{error::Error, sync::Arc};
-use tokio::sync::mpsc;
-use tokio_stream::wrappers::ReceiverStream;
-use tonic::{transport::Server, Request, Response, Status, Streaming};
+use std::{error::Error, sync::Arc, thread};
+use tonic::transport::Server;
 
-use sc_common::proto::{
-  minecraft_server::{Minecraft, MinecraftServer},
-  Packet, ReserveSlotsRequest, ReserveSlotsResponse, StatusRequest, StatusResponse,
-};
+use sc_server::{net::ConnectionManager, world::WorldManager};
 
-use sc_server::world::WorldManager;
-
-#[derive(Clone)]
-pub struct ServerImpl {
-  worlds: Arc<WorldManager>,
-}
-
-#[tonic::async_trait]
-impl Minecraft for ServerImpl {
-  type ConnectionStream = ReceiverStream<Result<Packet, Status>>;
-  async fn connection(
-    &self,
-    req: Request<Streaming<Packet>>,
-  ) -> Result<Response<Self::ConnectionStream>, Status> {
-    let (tx, rx) = mpsc::channel(8);
-
-    // We need to wait for a packet to be recieved from the proxy before we can
-    // create the player (we need a username and uuid). Therefore, we need to do
-    // this on another task.
-    let worlds = self.worlds.clone();
-    tokio::spawn(async move {
-      worlds.new_player(req.into_inner(), tx).await;
-    });
-
-    Ok(Response::new(ReceiverStream::new(rx)))
-  }
-  async fn status(&self, req: Request<StatusRequest>) -> Result<Response<StatusResponse>, Status> {
-    dbg!(req);
-    Ok(Response::new(StatusResponse {
-      id:          vec![],
-      num_players: 5,
-      server_type: "sugarcane-rs".into(),
-    }))
-  }
-  async fn reserve_slots(
-    &self,
-    req: Request<ReserveSlotsRequest>,
-  ) -> Result<Response<ReserveSlotsResponse>, Status> {
-    dbg!(req);
-    Ok(Response::new(ReserveSlotsResponse::default()))
-  }
-}
+// #[derive(Clone)]
+// pub struct ServerImpl {
+//   worlds: Arc<WorldManager>,
+// }
+//
+// #[tonic::async_trait]
+// impl Minecraft for ServerImpl {
+//   type ConnectionStream = ReceiverStream<Result<Packet, Status>>;
+//   async fn connection(
+//     &self,
+//     req: Request<Streaming<Packet>>,
+//   ) -> Result<Response<Self::ConnectionStream>, Status> {
+//     let (tx, rx) = mpsc::channel(8);
+//
+//     // We need to wait for a packet to be recieved from the proxy before we
+// can     // create the player (we need a username and uuid). Therefore, we
+// need to do     // this on another task.
+//     let worlds = self.worlds.clone();
+//     tokio::spawn(async move {
+//       worlds.new_player(req.into_inner(), tx).await;
+//     });
+//
+//     Ok(Response::new(ReceiverStream::new(rx)))
+//   }
+//   async fn status(&self, req: Request<StatusRequest>) ->
+// Result<Response<StatusResponse>, Status> {     dbg!(req);
+//     Ok(Response::new(StatusResponse {
+//       id:          vec![],
+//       num_players: 5,
+//       server_type: "sugarcane-rs".into(),
+//     }))
+//   }
+//   async fn reserve_slots(
+//     &self,
+//     req: Request<ReserveSlotsRequest>,
+//   ) -> Result<Response<ReserveSlotsResponse>, Status> {
+//     dbg!(req);
+//     Ok(Response::new(ReserveSlotsResponse::default()))
+//   }
+// }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -60,29 +53,16 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
   let addr = "0.0.0.0:8483".parse().unwrap();
 
-  let worlds = Arc::new(WorldManager::new());
-  worlds.add_world().await;
+  let wm = Arc::new(WorldManager::new());
+  wm.add_world();
 
-  let w = worlds.clone();
-  tokio::spawn(async move {
-    w.run().await;
-  });
-  let svc = MinecraftServer::new(ServerImpl { worlds });
+  let w = wm.clone();
+  thread::spawn(|| w.run());
 
-  // This is the code needed for reflection. It is disabled for now, as
-  // tonic-reflection does not allow you to disable rustfmt. For docker builds,
-  // rustfmt is not installed.
-  //
-  // let desc = tonic_reflection::server::Builder::configure()
-  //   .register_encoded_file_descriptor_set(common::proto::FILE_DESCRIPTOR_SET)
-  //   .build()?;
-  //
-  // Server::builder()
-  //   .add_service(svc)
-  //   .add_service(desc)
-  //   .serve(addr).await?;
+  let mut conn = ConnectionManager::new(wm);
 
   info!("listening on {}", addr);
-  Server::builder().add_service(svc).serve(addr).await?;
+  conn.run(addr)?;
+
   Ok(())
 }
