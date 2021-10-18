58fb4eed67bf88349283563f92a48799585499ff
Neil Macneale <neil.macneale.v@gmail.com>
2021-10-17 20:31:45 -0700

    Added docs and a value for how yaml keys are parsed

--- sc_common/src/config/mod.rs
+++ sc_common/src/config/mod.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fmt, fs};
 use yaml_rust::{yaml::Yaml, YamlLoader};
 
 pub struct Config {
@@ -18,13 +18,64 @@ pub trait YamlValue<'a> {
     Self: Sized;
 }
 
+/// A yaml key. This is how a path to a yaml value can be specified. This can be
+/// represented as either an array or a string. If it is a string, it will be
+/// split by dots into an array.
+///
+/// In order to index into maps, simply use a string name for a section. To
+/// index into an array, use a number in the array. Example:
+///
+/// ```
+/// foo: bar
+/// hello:
+///   name: world
+///   times: 1
+/// items:
+///   - 3
+///   - 4
+///   - a: 1
+///     lot: 10
+///     more: 100
+///     things: 1000
+/// ```
+///
+/// These are valid indices:
+/// ```
+/// foo         // points to 'bar'
+/// hello.name  // points to 'world'
+/// items.0     // points to 3
+/// items.2.lot // points to 10
+/// ```
+pub trait YamlKey {
+  /// Returns the sections of this key.
+  fn sections(&self) -> Vec<&str>;
+}
+
+impl YamlKey for &str {
+  fn sections(&self) -> Vec<&str> {
+    self.split('.').collect()
+  }
+}
+impl YamlKey for String {
+  fn sections(&self) -> Vec<&str> {
+    self.split('.').collect()
+  }
+}
+impl YamlKey for Vec<&str> {
+  fn sections(&self) -> Vec<&str> {
+    self.clone()
+  }
+}
+
 impl Config {
   /// Creates a new config for the given path. The path is a runtime path to
   /// load the config file. The default path is a secondary path, which will
   /// also be loaded. This will never be written to, and will be used as a
   /// fallback if the key doesn't exist in the file.
   pub fn new(path: &str, default: &str) -> Self {
-    Config { primary: Self::load_yaml(path), default: Self::load_yaml(default) }
+    let conf = Config { primary: Self::load_yaml(path), default: Self::load_yaml(default) };
+    dbg!(&conf.primary);
+    conf
   }
   /// Creates a new config file, but with source strings, instead of paths. This
   /// is used in the proxy, which stores its default config in the binary, and
@@ -62,6 +113,8 @@ impl Config {
   /// type), then it will use the default config. If it doesn't exist there (or
   /// if it's the wrong type), this function will panic.
   ///
+  /// See [`YamlKey`] for details on how that is parsed.
+  ///
   /// In my opinion, a key should always exist when you try to load it. If there
   /// was a function like `get_opt`, which would only return a value when
   /// present, that would make it much more difficult for users to find out what
@@ -73,15 +126,21 @@ impl Config {
   /// your own type. I hightly recommend against this, as that will just cause
   /// confusion for your users. I will not be adding any more implementations
   /// than the ones present in this file.
-  pub fn get<'a, T>(&'a self, key: &str) -> T
+  pub fn get<'a, K, T>(&'a self, key: &K) -> T
   where
+    K: YamlKey,
     T: YamlValue<'a>,
   {
     let val = &self.primary["hello"];
     match T::from_yaml(val) {
       Some(v) => v,
       None => {
-        warn!("invalid yaml value at `{}`: {:?}, expected a {}", key, val, T::name());
+        warn!(
+          "invalid yaml value at `{}`: {:?}, expected a {}",
+          key.sections().join("."),
+          val,
+          T::name()
+        );
         self.get_default(key)
       }
     }
@@ -89,15 +148,21 @@ impl Config {
 
   /// Gets the default value at the given key. This will panic if the key does
   /// not exist, or if it was the wrong type.
-  pub fn get_default<'a, T>(&'a self, key: &str) -> T
+  pub fn get_default<'a, K, T>(&'a self, key: &K) -> T
   where
+    K: YamlKey,
     T: YamlValue<'a>,
   {
     let val = &self.default["hello"];
     match T::from_yaml(val) {
       Some(v) => v,
       None => {
-        panic!("default had wrong type for key `{}`: {:?}, expected a {}", key, val, key);
+        panic!(
+          "default had wrong type for key `{}`: {:?}, expected a {}",
+          key.sections().join("."),
+          val,
+          T::name(),
+        );
       }
     }
   }
