626b14b4cb89a1683514931c89a83e5a78038cfc
Neil Macneale <neil.macneale.v@gmail.com>
2021-10-18 15:06:45 -0700

    Added a check for if an entity is living

--- sc_server/src/world/entities.rs
+++ sc_server/src/world/entities.rs
@@ -22,35 +22,69 @@ impl World {
   fn send_entity_spawn(&self, player: &Player, ent: &Entity) {
     let p = ent.pos();
     let id = ent.ty().id();
-    info!("modern id: {}", id);
-    let ty_8;
-    let ty_11;
-    if player.ver() > ProtocolVersion::V1_11 {
-      ty_11 = Some(self.entity_converter().to_old(id, player.ver().block()) as i32);
-      ty_8 = None;
+    if ent.ty().is_living() {
+      let ty_8;
+      let ty_11;
+      if player.ver() > ProtocolVersion::V1_11 {
+        ty_11 = Some(self.entity_converter().to_old(id, player.ver().block()) as i32);
+        ty_8 = None;
+      } else {
+        ty_8 = Some(self.entity_converter().to_old(id, player.ver().block()) as u8);
+        ty_11 = None;
+      }
+      info!("modern id: {}", id);
+      info!("old id: {:?}", ty_8);
+      player.send(cb::Packet::SpawnEntityLiving {
+        entity_id:              ent.eid(),
+        entity_uuid_v1_9:       Some(UUID::from_u128(0)),
+        type_v1_8:              ty_8,
+        type_v1_11:             ty_11,
+        x_v1_8:                 Some(p.fixed_x()),
+        x_v1_9:                 Some(p.x()),
+        y_v1_8:                 Some(p.fixed_y()),
+        y_v1_9:                 Some(p.y()),
+        z_v1_8:                 Some(p.fixed_z()),
+        z_v1_9:                 Some(p.z()),
+        yaw:                    0,
+        pitch:                  0,
+        head_pitch:             0,
+        velocity_x:             0,
+        velocity_y:             0,
+        velocity_z:             0,
+        metadata_removed_v1_15: Some(vec![0x7f]),
+      });
     } else {
-      ty_8 = Some(self.entity_converter().to_old(id, player.ver().block()) as u8);
-      ty_11 = None;
+      let ty_8;
+      let ty_14;
+      if player.ver() > ProtocolVersion::V1_14 {
+        ty_14 = Some(self.entity_converter().to_old(id, player.ver().block()) as i32);
+        ty_8 = None;
+      } else {
+        ty_8 = Some(self.entity_converter().to_old(id, player.ver().block()) as i8);
+        ty_14 = None;
+      }
+      // Data is some data specific to that entity. If it is non-zero, then velocity
+      // is present.
+      let data: i32 = 0;
+      player.send(cb::Packet::SpawnEntity {
+        entity_id:        ent.eid(),
+        object_uuid_v1_9: Some(UUID::from_u128(0)),
+        type_v1_8:        ty_8,
+        type_v1_14:       ty_14,
+        x_v1_8:           Some(p.fixed_x()),
+        x_v1_9:           Some(p.x()),
+        y_v1_8:           Some(p.fixed_y()),
+        y_v1_9:           Some(p.y()),
+        z_v1_8:           Some(p.fixed_z()),
+        z_v1_9:           Some(p.z()),
+        pitch:            0,
+        yaw:              0,
+        object_data_v1_8: Some(data.to_le_bytes().to_vec()),
+        object_data_v1_9: Some(data),
+        velocity_x_v1_9:  None,
+        velocity_y_v1_9:  None,
+        velocity_z_v1_9:  None,
+      });
     }
-    info!("old id: {:?}", ty_8);
-    player.send(cb::Packet::SpawnEntityLiving {
-      entity_id:              ent.eid(),
-      entity_uuid_v1_9:       Some(UUID::from_u128(0)),
-      type_v1_8:              ty_8,
-      type_v1_11:             ty_11,
-      x_v1_8:                 Some(p.fixed_x()),
-      x_v1_9:                 Some(p.x()),
-      y_v1_8:                 Some(p.fixed_y()),
-      y_v1_9:                 Some(p.y()),
-      z_v1_8:                 Some(p.fixed_z()),
-      z_v1_9:                 Some(p.z()),
-      yaw:                    0,
-      pitch:                  0,
-      head_pitch:             0,
-      velocity_x:             0,
-      velocity_y:             0,
-      velocity_z:             0,
-      metadata_removed_v1_15: Some(vec![0x7f]),
-    });
   }
 }
