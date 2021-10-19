822ee83ee1888392a4bf4fc570a99b6bd3e11486
Neil Macneale <neil.macneale.v@gmail.com>
2021-10-18 21:23:45 -0700

    Can determine when a collision will happen

--- sc_common/src/math/aabb.rs
+++ sc_common/src/math/aabb.rs
@@ -1,5 +1,6 @@
 use super::{FPos, Vec3};
 
+#[derive(Debug, Clone, Copy)]
 pub struct AABB {
   pos:  FPos,
   // Never negative
@@ -13,14 +14,17 @@ impl AABB {
     AABB { pos, size: Vec3::new(size.x.max(0.0), size.y.max(0.0), size.z.max(0.0)) }
   }
 
-  /// Performs a collision with the other bounding box. This will move self such
-  /// that self and other don't intersect. This should be called right after
-  /// moving self. Returns true if self was moved.
-  pub fn collide(&mut self, other: AABB) -> bool {
-    if !self.is_colliding_with(other) {
-      return false;
+  /// Moves this box in the given direction, and make sure that it doesn't
+  /// intersect with any of the given collision boxes.
+  pub fn move_towards(&mut self, delta: Vec3, nearby: &[AABB]) {
+    for &o in nearby {
+      let d = self.distance_from(o);
+      if d.x.abs() >= delta.x.abs() && d.y.abs() >= delta.y.abs() && d.z.abs() >= delta.z.abs() {
+        continue;
+      }
     }
-    false
+
+    self.pos += delta;
   }
 
   /// Returns true if self and other are intersecting. Being next to other
@@ -32,6 +36,27 @@ impl AABB {
       || (self.max_y() > other.min_y() && self.max_y() < other.max_y())
       || (self.min_z() > other.min_z() && self.min_z() < other.max_z())
       || (self.max_z() > other.min_z() && self.max_z() < other.max_z())
+  }
+
+  /// Returns the distance from the other AABB in all axis.
+  pub fn distance_from(&self, other: AABB) -> Vec3 {
+    Vec3::new(
+      if self.pos.x() > other.pos.x() {
+        other.max_x() - self.min_x()
+      } else {
+        self.max_x() - other.min_x()
+      },
+      if self.pos.y() > other.pos.y() {
+        other.max_y() - self.min_y()
+      } else {
+        self.max_y() - other.min_y()
+      },
+      if self.pos.z() > other.pos.z() {
+        other.max_z() - self.min_z()
+      } else {
+        self.max_z() - other.min_z()
+      },
+    )
   }
 
   pub fn min_x(&self) -> f64 {
