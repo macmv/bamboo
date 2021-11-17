e135f3b9e2d24cad3ee0d8b7d90006d9fe47f0f7
Neil Macneale <neil.macneale.v@gmail.com>
2021-11-16 19:41:34 -0800

    Can write a seperate enum variant for each version of each packet

--- sc_data/src/protocol/gen.rs
+++ sc_data/src/protocol/gen.rs
@@ -3,53 +3,75 @@ use crate::{gen::CodeGen, Version};
 use std::{collections::HashMap, fs, fs::File, io, io::Write, path::Path};
 
 pub fn generate(def: Vec<(Version, PacketDef)>, dir: &Path) -> io::Result<()> {
-  let mut all_cb_packets = HashMap::new();
-  let mut all_sb_packets = HashMap::new();
+  let mut all_cb_packets = PacketCollection::new();
+  let mut all_sb_packets = PacketCollection::new();
 
   for (ver, def) in def {
     for p in def.clientbound {
-      all_cb_packets.insert(p.name.clone(), (ver, p));
+      all_cb_packets.add(ver, p);
     }
     for p in def.serverbound {
-      all_sb_packets.insert(p.name.clone(), (ver, p));
+      all_sb_packets.add(ver, p);
     }
   }
 
-  let mut all_cb_packets: Vec<_> = all_cb_packets.into_iter().collect();
-  all_cb_packets.sort_unstable_by(|(a, _), (b, _)| a.cmp(b));
-  let mut all_sb_packets: Vec<_> = all_sb_packets.into_iter().collect();
-  all_sb_packets.sort_unstable_by(|(a, _), (b, _)| a.cmp(b));
-
   fs::create_dir_all(dir)?;
-  File::create(dir.join("cb.rs"))?.write_all(process(all_cb_packets).as_bytes())?;
-  File::create(dir.join("sb.rs"))?.write_all(process(all_sb_packets).as_bytes())?;
+  File::create(dir.join("cb.rs"))?.write_all(all_cb_packets.gen().as_bytes())?;
+  File::create(dir.join("sb.rs"))?.write_all(all_sb_packets.gen().as_bytes())?;
 
   Ok(())
 }
+struct PacketCollection {
+  packets: HashMap<String, Vec<(Version, Packet)>>,
+}
 
-fn process(packets: Vec<(String, (Version, Packet))>) -> String {
-  let mut gen = CodeGen::new();
-
-  gen.write("pub enum Packet ");
-  gen.write_block(|gen| {
-    for (name, (ver, p)) in &packets {
-      gen.write(name);
-      gen.write_line(" {");
-      gen.add_indent();
-      for f in &p.fields {
-        if f.name == "type" {
-          gen.write("ty");
-        } else {
-          gen.write(&f.name);
-        }
-        gen.write(": ");
-        gen.write(&f.ty.to_rust());
-        gen.write_line(",");
+impl PacketCollection {
+  pub fn new() -> Self {
+    PacketCollection { packets: HashMap::new() }
+  }
+  pub fn add(&mut self, ver: Version, p: Packet) {
+    let list = self.packets.entry(p.name.clone()).or_insert_with(|| vec![]);
+    if let Some((_, last)) = list.last() {
+      if *last == p {
+        return;
       }
-      gen.remove_indent();
-      gen.write_line("},");
     }
-  });
+    list.push((ver, p));
+  }
+  pub fn gen(self) -> String {
+    let mut gen = CodeGen::new();
 
-  gen.into_output()
+    let mut packets: Vec<_> = self.packets.into_iter().collect();
+    packets.sort_unstable_by(|(a, _), (b, _)| a.cmp(b));
+    let packets: Vec<Vec<(_, _)>> = packets.into_iter().map(|(_, v)| v).collect();
+
+    gen.write("pub enum Packet ");
+    gen.write_block(|gen| {
+      for versions in &packets {
+        for (ver, p) in versions {
+          write_packet(gen, &format!("{}V{}", p.name, ver.maj), p);
+        }
+      }
+    });
+
+    gen.into_output()
+  }
+}
+
+fn write_packet(gen: &mut CodeGen, name: &str, p: &Packet) {
+  gen.write(name);
+  gen.write_line(" {");
+  gen.add_indent();
+  for f in &p.fields {
+    if f.name == "type" {
+      gen.write("ty");
+    } else {
+      gen.write(&f.name);
+    }
+    gen.write(": ");
+    gen.write(&f.ty.to_rust());
+    gen.write_line(",");
+  }
+  gen.remove_indent();
+  gen.write_line("},");
 }
