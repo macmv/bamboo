62be118f3b859c2d07a99d462f6e30543abf0cff
Neil Macneale <neil.macneale.v@gmail.com>
2021-11-17 20:57:31 -0800

    Added a clamp function

--- sc_generated/src/net/mod.rs
+++ sc_generated/src/net/mod.rs
@@ -1,13 +1,27 @@
 // data::generate_protocol!();
 
 pub mod cb {
+  use super::clamp;
+
   include!(concat!(env!("OUT_DIR"), "/protocol/cb.rs"));
 }
 pub mod sb {
+  use super::clamp;
+
   include!(concat!(env!("OUT_DIR"), "/protocol/sb.rs"));
 }
 
 mod other;
 pub mod tcp;
+
+pub fn clamp<T: Ord + Copy>(mut a: T, min: T, max: T) -> T {
+  if a < min {
+    a = min
+  }
+  if a > max {
+    a = max
+  }
+  a
+}
 
 // pub use other::Other;
