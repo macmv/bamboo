dad8966475e6d1c117a47775f0a5f68dd7cfc3b5
Neil Macneale <neil.macneale.v@gmail.com>
2021-12-04 22:52:08 -0800

    tcp::Packet fields are converted into U (they should be Vec<u8>)

--- sc_data/src/protocol/gen.rs
+++ sc_data/src/protocol/gen.rs
@@ -85,6 +85,15 @@ impl PacketCollection {
         // eprintln!("finding reader type of {} for ver {}", p.name, v);
         // dbg!(&p);
         p.find_reader_types();
+        for f in &mut p.fields {
+          if let Some(reader_ty) = &mut f.reader_type {
+            if reader_ty.name == "tcp::Packet" {
+              reader_ty.name = "U".into();
+            } else if reader_ty.name == "Option" && reader_ty.generics[0].name == "tcp::Packet" {
+              reader_ty.generics[0].name = "U".into();
+            }
+          }
+        }
         p.generate_writer();
       }
     }
