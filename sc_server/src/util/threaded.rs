79772b375640aff7b23c4f37595c8e43454d9b5f
Neil Macneale <neil.macneale.v@gmail.com>
2021-12-12 16:51:20 -0800

    Added docs and a return value for the Threaded type

--- sc_server/src/util/threaded.rs
+++ sc_server/src/util/threaded.rs
@@ -12,11 +12,17 @@ pub struct Threaded<T> {
 }
 
 impl<T> Threaded<T> {
+  /// Creates a new threaded type. Anytime [`get`](Self::get) is called on a new
+  /// thread, the `builder` function will be called. This should always return
+  /// the same object on every thread. It will still function if you don't
+  /// return the same object, as calling `get` from the same thread will always
+  /// return the stored object for that thread.
   pub fn new(builder: impl Fn() -> T + 'static) -> Self {
     Threaded { threads: RwLock::new(HashMap::new()), builder: Box::new(builder) }
   }
 
-  pub fn get(&self, run: impl Fn(&mut T)) {
+  /// Calls `run`, and passes the item stored for the current thread.
+  pub fn get<R>(&self, run: impl FnOnce(&mut T) -> R) -> R {
     let tid = thread::current().id();
     if !self.threads.read().contains_key(&tid) {
       self.threads.write().insert(tid, RefCell::new((self.builder)()));
@@ -24,6 +30,7 @@ impl<T> Threaded<T> {
 
     let threads = self.threads.read();
     let item = &threads[&tid];
-    run(&mut item.borrow_mut());
+    let r = run(&mut item.borrow_mut());
+    r
   }
 }
