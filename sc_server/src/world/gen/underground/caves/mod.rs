a3a0a3435beaa59c523393c3d52927ed089d7728
Neil Macneale <neil.macneale.v@gmail.com>
2021-12-17 19:54:24 -0800

    Moved cave noise into another file

--- sc_server/src/world/gen/underground/caves/mod.rs
+++ sc_server/src/world/gen/underground/caves/mod.rs
@@ -3,32 +3,24 @@ use super::super::{
   util::Cache,
   WorldGen,
 };
-use crate::{block, util::Threaded, world::chunk::MultiChunk};
-use noise::{BasicMulti, NoiseFn};
+use crate::{util::Threaded, world::chunk::MultiChunk};
 use sc_common::math::{ChunkPos, Pos};
 
+mod noise;
 mod worm;
-pub use worm::CaveWorm;
+pub use self::{noise::CaveNoise, worm::CaveWorm};
 
 pub struct CaveGen {
   origins: PointGrid,
   worms:   Threaded<Cache<Point, CaveWorm>>,
-  noise:   BasicMulti,
-  middle:  BasicMulti,
-  offset:  BasicMulti,
+  noise:   CaveNoise,
 }
 
 impl CaveGen {
   pub fn new(seed: u64) -> Self {
-    let mut noise = BasicMulti::new();
-    noise.octaves = 5;
-    let mut middle = BasicMulti::new();
-    middle.octaves = 3;
-    let mut offset = BasicMulti::new();
-    offset.octaves = 1;
     CaveGen {
       origins: PointGrid::new(seed, 256, 64),
-      worms: Threaded::new(move || {
+      worms:   Threaded::new(move || {
         Cache::new(move |origin: Point| {
           let mut worm = CaveWorm::new(
             seed ^ ((origin.x as u64) << 32) ^ origin.y as u64,
@@ -38,59 +30,15 @@ impl CaveGen {
           worm
         })
       }),
-      noise,
-      middle,
-      offset,
+      noise:   CaveNoise::new(seed),
     }
   }
 
   pub fn carve(&self, _world: &WorldGen, pos: ChunkPos, c: &mut MultiChunk) {
-    self.carve_cave_noise(pos, c);
+    self.noise.carve(pos, c);
     for origin in self.origins.neighbors(Point::new(pos.block_x(), pos.block_z()), 1) {
       self.carve_cave_worm(origin, pos, c);
       // self.carve_cave_tree(origin, pos, c);
-    }
-  }
-
-  fn carve_cave_noise(&self, pos: ChunkPos, c: &mut MultiChunk) {
-    let min_height = 0.0_f64;
-    let max_height = 32.0_f64;
-    let b_min_height = 0;
-    let b_max_height = 48;
-
-    for p in pos.columns() {
-      let middle = (self.middle.get([p.x() as f64, p.z() as f64]) + 1.0) / 2.0 * 32.0;
-      let b_middle = middle as i32;
-      for y in b_min_height..=b_max_height {
-        let rel = p.chunk_rel().with_y(y);
-        let val = {
-          const DIV: f64 = 32.0;
-          let x = p.x() as f64 / DIV;
-          let y = y as f64 / DIV;
-          let z = p.z() as f64 / DIV;
-          self.noise.get([x, y, z])
-        };
-        let offset = {
-          const DIV: f64 = 256.0;
-          let x = p.x() as f64 / DIV;
-          let y = y as f64 / DIV;
-          let z = p.z() as f64 / DIV;
-          (self.offset.get([x, y, z]) + 1.0) / 2.0
-        };
-        let a = (y as f64 - min_height) / (max_height - min_height);
-        let b = if y > b_middle { 0.0 } else { (b_middle - y) as f64 / middle };
-        // This is a gradient from top to bottom for how much open space we want.
-        let v = a + b;
-        // This converts that gradient into a value that we will use to dampen the 3D
-        // noise map.
-        let mut min = v * 0.2 - 0.1;
-        // This dampens the noise more using another noise map, so that we only have
-        // caverns in some areas.
-        min += offset * 0.1;
-        if val > min {
-          c.set_kind(rel, block::Kind::Air).unwrap();
-        }
-      }
     }
   }
 
---
+++ sc_server/src/world/gen/underground/caves/noise.rs
@@ -0,0 +1,65 @@
+use crate::{block, world::chunk::MultiChunk};
+use noise::{BasicMulti, NoiseFn};
+use sc_common::math::{ChunkPos, Pos};
+
+pub struct CaveNoise {
+  noise:  BasicMulti,
+  middle: BasicMulti,
+  offset: BasicMulti,
+}
+
+impl CaveNoise {
+  pub fn new(seed: u64) -> Self {
+    let mut noise = BasicMulti::new();
+    noise.octaves = 5;
+    let mut middle = BasicMulti::new();
+    middle.octaves = 3;
+    let mut offset = BasicMulti::new();
+    offset.octaves = 1;
+    CaveNoise { noise, middle, offset }
+  }
+
+  fn val(&self, pos: Pos) -> f64 { 0.0 }
+
+  pub fn carve(&self, pos: ChunkPos, c: &mut MultiChunk) {
+    let min_height = 0.0_f64;
+    let max_height = 32.0_f64;
+    let b_min_height = 0;
+    let b_max_height = 48;
+
+    for p in pos.columns() {
+      let middle = (self.middle.get([p.x() as f64, p.z() as f64]) + 1.0) / 2.0 * 32.0;
+      let b_middle = middle as i32;
+      for y in b_min_height..=b_max_height {
+        let rel = p.chunk_rel().with_y(y);
+        let val = {
+          const DIV: f64 = 32.0;
+          let x = p.x() as f64 / DIV;
+          let y = y as f64 / DIV;
+          let z = p.z() as f64 / DIV;
+          self.noise.get([x, y, z])
+        };
+        let offset = {
+          const DIV: f64 = 256.0;
+          let x = p.x() as f64 / DIV;
+          let y = y as f64 / DIV;
+          let z = p.z() as f64 / DIV;
+          (self.offset.get([x, y, z]) + 1.0) / 2.0
+        };
+        let a = (y as f64 - min_height) / (max_height - min_height);
+        let b = if y > b_middle { 0.0 } else { (b_middle - y) as f64 / middle };
+        // This is a gradient from top to bottom for how much open space we want.
+        let v = a + b;
+        // This converts that gradient into a value that we will use to dampen the 3D
+        // noise map.
+        let mut min = v * 0.2 - 0.1;
+        // This dampens the noise more using another noise map, so that we only have
+        // caverns in some areas.
+        min += offset * 0.1;
+        if val > min {
+          c.set_kind(rel, block::Kind::Air).unwrap();
+        }
+      }
+    }
+  }
+}
