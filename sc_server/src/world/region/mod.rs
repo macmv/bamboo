5a952f60774d76add76a2b00a1c2dd8a6f4727fb
Neil Macneale <neil.macneale.v@gmail.com>
2022-02-01 03:29:14 -0800

    Can read region files from disk

--- sc_server/src/world/region/mod.rs
+++ sc_server/src/world/region/mod.rs
@@ -44,9 +44,27 @@ impl World {
     Ok(())
   }
 
-  fn load_region_file(&self, x: i32, z: i32, path: &Path) -> io::Result<()> {
-    let nbt = NBT::deserialize_file(fs::read(path)?);
-    dbg!(&nbt);
+  fn load_region_file(&self, region_x: i32, region_z: i32, path: &Path) -> io::Result<()> {
+    let data = fs::read(path)?;
+    let header = &data[..8192];
+    // `offset` is an offset into the file, not an offset into the chunks table.
+    let chunks = &data;
+    for id in 0..1024 {
+      let start = id * 4;
+      let num = u32::from_be_bytes(header[start..start + 4].try_into().unwrap());
+      let offset: usize = ((num >> 8) & 0xffffff) as usize * 4096;
+      let size: usize = (num & 0xff) as usize * 4096;
+      if size == 0 {
+        continue;
+      }
+
+      let chunk = &chunks[offset..offset + size];
+      let header = &chunk[..5];
+      let len = u32::from_be_bytes(header[..4].try_into().unwrap()) as usize;
+      let _compression = header[4];
+      let nbt = NBT::deserialize_file(chunk[5..5 + len].to_vec());
+      dbg!(&nbt);
+    }
     Ok(())
   }
 }
