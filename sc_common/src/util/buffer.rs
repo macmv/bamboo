d1b4cb9451050fe999d6315ec272cac464976cc1
Neil Macneale <neil.macneale.v@gmail.com>
2022-02-07 16:06:32 -0800

    Buffer now returns a Result from all the reader functions

--- sc_common/src/util/buffer.rs
+++ sc_common/src/util/buffer.rs
@@ -10,16 +10,26 @@ use std::{
   string::FromUtf8Error,
 };
 
+pub type Result<T> = std::result::Result<T, BufferError>;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Mode {
+  Reading,
+  Writing,
+}
+
+use Mode::{Reading, Writing};
+
 #[derive(Debug)]
 pub struct BufferError {
-  err:     BufferErrorKind,
-  pos:     u64,
-  reading: bool,
+  err:  BufferErrorKind,
+  pos:  u64,
+  mode: Mode,
 }
 
 impl fmt::Display for BufferError {
   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-    if self.reading {
+    if self.mode == Mode::Reading {
       write!(f, "error while reading from buffer at index {}: {}", self.pos, self.err)
     } else {
       write!(f, "error while writing to buffer at index {}: {}", self.pos, self.err)
@@ -35,6 +45,7 @@ pub enum BufferErrorKind {
   StringTooLong { len: u64, max: u64 },
   ArrayTooLong { len: u64, max: u64 },
   NegativeLen(i32),
+  Expected(Vec<u8>, Vec<u8>),
 }
 
 impl fmt::Display for BufferErrorKind {
@@ -50,120 +61,81 @@ impl fmt::Display for BufferErrorKind {
         write!(f, "array is `{}` elements, longer than max `{}`", len, max)
       }
       Self::NegativeLen(len) => write!(f, "len `{}` is negative", len),
+      Self::Expected(expected, got) => write!(f, "expected bytes {:x?}, got {:x?}", expected, got),
     }
   }
 }
 
 impl Error for BufferError {}
-impl Error for BufferErrorKind {}
+
+impl From<io::Error> for BufferErrorKind {
+  fn from(e: io::Error) -> Self { BufferErrorKind::IO(e) }
+}
+impl From<FromUtf8Error> for BufferErrorKind {
+  fn from(e: FromUtf8Error) -> Self { BufferErrorKind::FromUtf8Error(e) }
+}
 
 #[derive(Debug)]
 pub struct Buffer<'a> {
   data: Cursor<&'a mut Vec<u8>>,
-  err:  Option<BufferError>,
 }
 
 macro_rules! add_read {
   ($fn: ident, $ty: ty, $zero: expr) => {
-    pub fn $fn(&mut self) -> $ty {
-      if self.err.is_some() {
-        return $zero;
-      }
-      match self.data.$fn::<BigEndian>() {
-        Ok(v) => v,
-        Err(e) => {
-          self.set_err(BufferErrorKind::IO(e), true);
-          $zero
-        }
-      }
+    pub fn $fn(&mut self) -> Result<$ty> {
+      self.data.$fn::<BigEndian>().map_err(|e| self.err(e, Reading))
     }
   };
 }
 // The same as add_read(), but with no type parameter
 macro_rules! add_read_byte {
   ($fn: ident, $ty: ty) => {
-    pub fn $fn(&mut self) -> $ty {
-      if self.err.is_some() {
-        return 0;
-      }
-      match self.data.$fn() {
-        Ok(v) => v,
-        Err(e) => {
-          self.set_err(BufferErrorKind::IO(e), true);
-          0
-        }
-      }
-    }
+    pub fn $fn(&mut self) -> Result<$ty> { self.data.$fn().map_err(|e| self.err(e, Reading)) }
   };
 }
 
 macro_rules! add_write {
   ($fn: ident, $ty: ty) => {
     pub fn $fn(&mut self, v: $ty) {
-      if self.err.is_some() {
-        return;
-      }
-      match self.data.$fn::<BigEndian>(v) {
-        Ok(()) => {}
-        Err(e) => {
-          self.set_err(BufferErrorKind::IO(e), false);
-        }
-      }
+      self.data.$fn::<BigEndian>(v).expect("could not write to buffer")
     }
   };
 }
-// The same as add_read(), but with no type parameter
+// The same as add_write(), but with no type parameter
 macro_rules! add_write_byte {
   ($fn: ident, $ty: ty) => {
-    pub fn $fn(&mut self, v: $ty) {
-      if self.err.is_some() {
-        return;
-      }
-      match self.data.$fn(v) {
-        Ok(()) => {}
-        Err(e) => {
-          self.set_err(BufferErrorKind::IO(e), false);
-        }
-      }
-    }
+    pub fn $fn(&mut self, v: $ty) { self.data.$fn(v).expect("could not write to buffer") }
   };
 }
 
 impl<'a> Buffer<'a> {
-  pub fn new(data: &'a mut Vec<u8>) -> Self { Buffer { data: Cursor::new(data), err: None } }
+  pub fn new(data: &'a mut Vec<u8>) -> Self { Buffer { data: Cursor::new(data) } }
   pub fn new_index(data: &'a mut Vec<u8>, index: usize) -> Self {
     let mut cursor = Cursor::new(data);
     cursor.set_position(index as u64);
-    Buffer { data: cursor, err: None }
+    Buffer { data: cursor }
   }
 
-  pub fn err(&self) -> &Option<BufferError> { &self.err }
-  pub fn set_err(&mut self, err: BufferErrorKind, reading: bool) {
-    self.err = Some(BufferError { err, pos: self.data.position(), reading });
+  pub fn err(&self, e: impl Into<BufferErrorKind>, mode: Mode) -> BufferError {
+    BufferError { err: e.into(), pos: self.data.position(), mode }
   }
 
   /// Writes all of data to the buffer. This will increment the position of the
   /// reader as well. Use append to write to the end of the buffer without
   /// changing position.
-  pub fn write(&mut self, data: &[u8]) {
-    match self.data.write(data) {
-      Ok(_) => {}
-      Err(e) => self.set_err(BufferErrorKind::IO(e), true),
-    };
+  pub fn write(&mut self, data: &[u8]) -> Result<()> {
+    self.data.write_all(data).map_err(|e| self.err(e, Writing))
   }
-  pub fn read(&mut self, len: usize) -> Vec<u8> {
+  pub fn read(&mut self, len: usize) -> Result<Vec<u8>> {
     let mut vec = vec![0u8; len];
-    match self.data.read(&mut vec) {
-      Ok(_) => {}
-      Err(e) => self.set_err(BufferErrorKind::IO(e), true),
-    }
-    vec
+    self.data.read_exact(&mut vec).map_err(|e| self.err(e, Reading))?;
+    Ok(vec)
   }
   pub fn len(&self) -> usize { self.data.get_ref().len() }
   pub fn is_empty(&self) -> bool { self.len() == 0 }
   pub fn index(&self) -> usize { usize::try_from(self.data.position()).unwrap() }
 
-  pub fn read_bool(&mut self) -> bool { self.read_u8() != 0 }
+  pub fn read_bool(&mut self) -> Result<bool> { Ok(self.read_u8()? != 0) }
   add_read_byte!(read_u8, u8);
   add_read!(read_u16, u16, 0);
   add_read!(read_u32, u32, 0);
@@ -176,10 +148,14 @@ impl<'a> Buffer<'a> {
   add_read!(read_f32, f32, 0.0);
   add_read!(read_f64, f64, 0.0);
 
-  pub fn expect(&mut self, expected: &[u8]) {
-    let got = self.read_buf(expected.len());
-    if got != expected {
-      panic!("expected {:?}, got {:?}", expected, got);
+  /// Expects the next bytes. Returns an error if the next bytes do not match.
+  /// This will read the exact number of bytes that are passed in.
+  pub fn expect(&mut self, expected: &[u8]) -> Result<()> {
+    let got = self.read_buf(expected.len())?;
+    if got == expected {
+      Ok(())
+    } else {
+      Err(self.err(BufferErrorKind::Expected(expected.to_vec(), got), Reading))
     }
   }
 
@@ -202,34 +178,24 @@ impl<'a> Buffer<'a> {
   add_write!(write_f32, f32);
   add_write!(write_f64, f64);
 
+  /// Doesn't return a result, as this will just return an empty array if we
+  /// have read anything.
   pub fn read_all(&mut self) -> Vec<u8> {
-    if self.err.is_some() {
-      return vec![];
-    }
     // TODO: Possibly change this limit
     let mut buf = vec![0; 1024];
     match self.data.read_to_end(&mut buf) {
       Ok(len) => buf[buf.len() - len..].to_vec(),
-      Err(e) => {
-        self.set_err(BufferErrorKind::IO(e), true);
-        vec![]
-      }
+      Err(e) => panic!("failed to read all: {:?}", e),
     }
   }
 
-  pub fn read_buf(&mut self, len: usize) -> Vec<u8> {
-    if self.err.is_some() {
-      return vec![];
-    }
+  pub fn read_buf(&mut self, len: usize) -> Result<Vec<u8>> {
     let mut buf = vec![0; len];
-    match self.data.read(&mut buf) {
-      Ok(_len) => buf.to_vec(),
-      Err(e) => {
-        self.set_err(BufferErrorKind::IO(e), true);
-        vec![]
-      }
-    }
+    self.data.read_exact(&mut buf).map_err(|e| self.err(e, Reading))?;
+    Ok(buf)
   }
+  /// This doesn't return a result, as the only thing that could go wrong is a
+  /// oom error, which isn't even returned as an error.
   pub fn write_buf(&mut self, v: &[u8]) { self.data.write_all(v).unwrap(); }
 
   /// This writes a fixed point floating number to the buffer. This simply
@@ -238,58 +204,36 @@ impl<'a> Buffer<'a> {
   pub fn write_fixed_int(&mut self, v: f64) { self.write_i32((v * 32.0) as i32); }
 
   /// Reads a string. If the length is longer than the given maximum, this will
-  /// fail, and return an empty string.
-  pub fn read_str(&mut self, max_len: u64) -> String {
-    if self.err.is_some() {
-      return "".into();
-    }
-    let len = self.read_varint();
-    let len = match len.try_into() {
-      Ok(v) => v,
-      Err(_) => {
-        self.set_err(BufferErrorKind::NegativeLen(len), true);
-        return "".into();
-      }
-    };
+  /// fail, and return an error.
+  pub fn read_str(&mut self, max_len: u64) -> Result<String> {
+    let len = self.read_varint()?;
+    let len = len.try_into().map_err(|e| self.err(BufferErrorKind::NegativeLen(len), Reading))?;
     if len > max_len * 4 {
-      self.set_err(BufferErrorKind::StringTooLong { len, max: max_len }, true);
-      return "".into();
+      return Err(self.err(BufferErrorKind::StringTooLong { len, max: max_len }, Reading));
     }
-    let vec = self.read(len as usize);
+    let vec = self.read(len as usize)?;
     match String::from_utf8(vec) {
       Ok(v) => {
         if v.len() > max_len as usize {
-          self.set_err(BufferErrorKind::StringTooLong { len, max: max_len }, true);
-          "".into()
+          Err(self.err(BufferErrorKind::StringTooLong { len, max: max_len }, Reading))
         } else {
-          v
+          Ok(v)
         }
       }
-      Err(e) => {
-        self.set_err(BufferErrorKind::FromUtf8Error(e), true);
-        "".into()
-      }
+      Err(e) => Err(self.err(e, Reading)),
     }
   }
   pub fn write_str(&mut self, v: &str) {
-    if self.err.is_some() {
-      return;
-    }
     self.write_varint(v.len() as i32);
     self.write(v.as_bytes());
   }
 
-  pub fn read_varint(&mut self) -> i32 {
-    if self.err.is_some() {
-      return 0;
-    }
+  pub fn read_varint(&mut self) -> Result<i32> {
     let mut res: i32 = 0;
     for i in 0..5 {
-      let read = self.read_u8();
+      let read = self.read_u8()?;
       if i == 4 && read & 0b10000000 != 0 {
-        // TODO: Custom error here
-        self.set_err(BufferErrorKind::VarInt, true);
-        return 0;
+        return Err(self.err(BufferErrorKind::VarInt, Reading));
       }
 
       let v = read & 0b01111111;
@@ -299,7 +243,7 @@ impl<'a> Buffer<'a> {
         break;
       }
     }
-    res
+    Ok(res)
   }
   pub fn write_varint(&mut self, v: i32) {
     // Need to work with u32, as >> acts differently on i32 vs u32.
@@ -323,19 +267,21 @@ impl<'a> Buffer<'a> {
     self.write_i32(p.z());
   }
   /// Reads a chunk position, as two i32s.
-  pub fn read_chunk_pos(&mut self) -> ChunkPos { ChunkPos::new(self.read_i32(), self.read_i32()) }
+  pub fn read_chunk_pos(&mut self) -> Result<ChunkPos> {
+    Ok(ChunkPos::new(self.read_i32()?, self.read_i32()?))
+  }
 
   /// Reads an nbt tag from self.
   pub fn read_nbt(&mut self) -> NBT { NBT::deserialize_buf(self).unwrap() }
 
   /// Reads a length prefixed array of integers.
-  pub fn read_i32_arr(&mut self) -> Vec<i32> {
-    let len = self.read_varint().try_into().unwrap();
+  pub fn read_i32_arr(&mut self) -> Result<Vec<i32>> {
+    let len = self.read_varint()?.try_into().unwrap();
     let mut out = Vec::with_capacity(len);
     for _ in 0..len {
-      out.push(self.read_i32());
+      out.push(self.read_i32()?);
     }
-    out
+    Ok(out)
   }
 
   pub fn write_i32_arr(&mut self, list: &[i32]) {
@@ -346,20 +292,22 @@ impl<'a> Buffer<'a> {
   }
 
   /// Reads 16 bytes from the buffer, and returns that as a big endian UUID.
-  pub fn read_uuid(&mut self) -> UUID { UUID::from_be_bytes(self.read_buf(16).try_into().unwrap()) }
+  pub fn read_uuid(&mut self) -> Result<UUID> {
+    Ok(UUID::from_be_bytes(self.read_buf(16)?.try_into().unwrap()))
+  }
 
   /// This writes a UUID into the buffer (in big endian format).
   pub fn write_uuid(&mut self, v: UUID) { self.write_buf(&v.as_be_bytes()); }
 
   /// Reads a list from the packet. This is new to 1.17, and simplifies a bunch
   /// of small for loops in previous versions.
-  pub fn read_list<T>(&mut self, val: impl Fn(&mut Buffer) -> T) -> Vec<T> {
-    let len = self.read_varint().try_into().unwrap();
+  pub fn read_list<T>(&mut self, val: impl Fn(&mut Buffer) -> Result<T>) -> Result<Vec<T>> {
+    let len = self.read_varint()?.try_into().unwrap();
     let mut list = Vec::with_capacity(len);
     for _ in 0..len {
-      list.push(val(self));
+      list.push(val(self)?);
     }
-    list
+    Ok(list)
   }
   /// Writes a list to the buffer.
   pub fn write_list<T>(&mut self, list: &[T], write: impl Fn(&mut Buffer, &T)) {
@@ -371,27 +319,31 @@ impl<'a> Buffer<'a> {
   /// Reads a list from the packet. If the length is greater than `max`, this
   /// fails. This is new to 1.17, and simplifies a bunch of small for loops in
   /// previous versions.
-  pub fn read_list_max<T>(&mut self, val: impl Fn(&mut Buffer) -> T, max: usize) -> Vec<T> {
-    let len: usize = self.read_varint().try_into().unwrap();
+  pub fn read_list_max<T>(
+    &mut self,
+    val: impl Fn(&mut Buffer) -> Result<T>,
+    max: usize,
+  ) -> Result<Vec<T>> {
+    let len: usize = self.read_varint()?.try_into().unwrap();
     if len > max {
-      self.set_err(BufferErrorKind::ArrayTooLong { len: len as u64, max: max as u64 }, true);
-      return vec![];
+      return Err(
+        self.err(BufferErrorKind::ArrayTooLong { len: len as u64, max: max as u64 }, Reading),
+      );
     }
     let mut list = Vec::with_capacity(len);
     for _ in 0..len {
-      list.push(val(self));
+      list.push(val(self)?);
     }
-    list
+    Ok(list)
   }
 
   /// Reads a boolean. If true, the closure is called, and the returned value is
   /// wrapped in Some. Otherwise, this returns None.
-  pub fn read_option<T>(&mut self, val: impl FnOnce(&mut Buffer) -> T) -> Option<T> {
-    if self.read_bool() {
-      Some(val(self))
-    } else {
-      None
-    }
+  pub fn read_option<T>(
+    &mut self,
+    val: impl FnOnce(&mut Buffer) -> Result<T>,
+  ) -> Result<Option<T>> {
+    Ok(if self.read_bool()? { Some(val(self)?) } else { None })
   }
   /// Writes `true` if the option is Some, or `false` if None. If the option is
   /// some, then it also calls the `write` closure.
@@ -403,7 +355,7 @@ impl<'a> Buffer<'a> {
     }
   }
 
-  pub fn read_varint_arr(&mut self) -> Vec<i32> { self.read_list(|buf| buf.read_varint()) }
+  pub fn read_varint_arr(&mut self) -> Result<Vec<i32>> { self.read_list(|buf| buf.read_varint()) }
 
   pub fn write_varint_arr(&mut self, v: &[i32]) { self.write_list(v, |p, &v| p.write_varint(v)) }
 }
@@ -426,28 +378,23 @@ mod tests {
   pub fn read_varint() {
     let mut data = vec![1];
     let mut buf = Buffer::new(&mut data);
-    assert_eq!(1, buf.read_varint());
-    assert!(buf.err().is_none());
+    assert_eq!(Ok(1), buf.read_varint());
 
     let mut data = vec![127];
     let mut buf = Buffer::new(&mut data);
-    assert_eq!(127, buf.read_varint());
-    assert!(buf.err().is_none());
+    assert_eq!(Ok(127), buf.read_varint());
 
     let mut data = vec![128, 2];
     let mut buf = Buffer::new(&mut data);
-    assert_eq!(256, buf.read_varint());
-    assert!(buf.err().is_none());
+    assert_eq!(Ok(256), buf.read_varint());
 
     let mut data = vec![255, 255, 255, 255, 15];
     let mut buf = Buffer::new(&mut data);
-    assert_eq!(-1, buf.read_varint());
-    assert!(buf.err().is_none());
+    assert_eq!(Ok(-1), buf.read_varint());
 
     let mut data = vec![255, 255, 255, 255, 255];
     let mut buf = Buffer::new(&mut data);
-    assert_eq!(0, buf.read_varint());
-    assert!(buf.err().is_some());
+    assert!(buf.read_varint().is_err());
   }
 
   #[test]
@@ -455,25 +402,21 @@ mod tests {
     let mut data = vec![];
     let mut buf = Buffer::new(&mut data);
     buf.write_varint(1);
-    assert!(buf.err().is_none());
     assert_eq!(vec![1], data);
 
     let mut data = vec![];
     let mut buf = Buffer::new(&mut data);
     buf.write_varint(127);
-    assert!(buf.err().is_none());
     assert_eq!(vec![127], data);
 
     let mut data = vec![];
     let mut buf = Buffer::new(&mut data);
     buf.write_varint(256);
-    assert!(buf.err().is_none());
     assert_eq!(vec![128, 2], data);
 
     let mut data = vec![];
     let mut buf = Buffer::new(&mut data);
     buf.write_varint(-1);
-    assert!(buf.err().is_none());
     assert_eq!(vec![255, 255, 255, 255, 15], data);
   }
 }
