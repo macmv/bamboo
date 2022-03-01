dbf6ee02574f92e5712cef961ba7f94dfc813188
Neil Macneale <neil.macneale.v@gmail.com>
2022-03-01 14:55:00 -0800

    Added item UI type. This will allow for easy creating of chest guis

--- sc_server/src/item/mod.rs
+++ sc_server/src/item/mod.rs
@@ -2,8 +2,10 @@ mod inventory;
 mod stack;
 mod ty;
 mod version;
+mod ui;
 
 pub use inventory::Inventory;
 pub use stack::Stack;
 pub use ty::{Data, Type};
+pub use ui::UI;
 pub use version::TypeConverter;
---
+++ sc_server/src/item/ui.rs
@@ -0,0 +1,50 @@
+use super::{Inventory, Stack};
+use std::collections::HashMap;
+use thiserror::Error;
+
+pub struct UI {
+  pattern: Vec<String>,
+  items:   HashMap<char, Stack>,
+}
+
+#[derive(Debug, Clone, Error)]
+pub enum UIError {
+  #[error("no rows were passed in")]
+  EmptyRows,
+  #[error("row `{row}` was `{len}` characters long, not `{expected_len}` characters")]
+  MismatchedRows { row: usize, len: usize, expected_len: usize },
+  #[error("missing item for key `{0}`")]
+  MissingItem(char),
+}
+
+impl UI {
+  pub fn new(pattern: Vec<String>) -> Result<UI, UIError> {
+    if pattern.is_empty() {
+      return Err(UIError::EmptyRows);
+    }
+    // TODO: Support things like dropper UIs
+    let expected_len = 9;
+    for (i, row) in pattern.iter().enumerate() {
+      let len = row.chars().count();
+      if len != expected_len {
+        return Err(UIError::MismatchedRows { row: i, len, expected_len });
+      }
+    }
+    Ok(UI { pattern, items: HashMap::new() })
+  }
+
+  pub fn item(&mut self, key: char, stack: Stack) { self.items.insert(key, stack); }
+
+  pub fn to_inventory(&self) -> Result<Inventory, UIError> {
+    let mut inv = Inventory::new(self.pattern.len() as u32 * 9);
+    for (r, row) in self.pattern.iter().enumerate() {
+      for (col, c) in row.chars().enumerate() {
+        match self.items.get(&c) {
+          Some(stack) => inv.set(r as u32 * 9 + col as u32, stack.clone()),
+          None => return Err(UIError::MissingItem(c)),
+        }
+      }
+    }
+    Ok(inv)
+  }
+}
