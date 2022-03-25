c692a4efd6af6c05ebd7219f4149154b6edf2626
Neil Macneale <neil.macneale.v@gmail.com>
2022-03-24 21:34:12 -0700

    MIO stuff compiles, with an event loop, which needs to be tested

--- sc_server/src/plugin/socket/mod.rs
+++ sc_server/src/plugin/socket/mod.rs
@@ -1,27 +1,33 @@
 use super::{PluginEvent, PluginImpl, ServerEvent};
 use crate::world::WorldManager;
 use crossbeam_channel::{Receiver, Sender};
-use mio::{net::UnixStream, Events, Interest, Poll, Token, Waker};
+use mio::{event::Event, net::UnixStream, Events, Interest, Poll, Token, Waker};
 use std::{
   collections::HashMap,
   fs, io,
-  io::{BufRead, BufReader},
+  io::{BufRead, BufReader, Read, Write},
   os::unix::net::UnixListener as StdUnixListener,
   path::{Path, PathBuf},
   process::{Command, Stdio},
   sync::Arc,
 };
 
+struct WrappedSocket {
+  stream:   UnixStream,
+  outgoing: Vec<u8>,
+  incoming: Vec<u8>,
+  plug_tx:  Sender<PluginEvent>,
+}
+
 pub struct SocketManager {
   wm:       Arc<WorldManager>,
-  sockets:  HashMap<Token, UnixStream>,
+  sockets:  HashMap<Token, WrappedSocket>,
   waker:    Arc<Waker>,
   next_tok: usize,
   poll:     Poll,
   tok_tx:   Sender<Token>,
   tok_rx:   Receiver<Token>,
   serv_rx:  HashMap<Token, Receiver<ServerEvent>>,
-  plug_tx:  HashMap<Token, Sender<PluginEvent>>,
   plugins:  Vec<Arc<SocketPlugin>>,
 }
 
@@ -34,10 +40,12 @@ pub struct SocketPlugin {
   rx:      Receiver<PluginEvent>,
 }
 
+const LISTEN: Token = Token(0);
+
 impl SocketManager {
   pub fn new(wm: Arc<WorldManager>) -> SocketManager {
     let poll = Poll::new().unwrap();
-    let waker = Waker::new(poll.registry(), Token(0)).unwrap();
+    let waker = Waker::new(poll.registry(), LISTEN).unwrap();
     let (tok_tx, tok_rx) = crossbeam_channel::bounded(128);
     SocketManager {
       wm,
@@ -48,7 +56,6 @@ impl SocketManager {
       tok_rx,
       tok_tx,
       serv_rx: HashMap::new(),
-      plug_tx: HashMap::new(),
       plugins: vec![],
     }
   }
@@ -74,9 +81,8 @@ impl SocketManager {
       .registry()
       .register(&mut socket, tok, Interest::READABLE | Interest::WRITABLE)
       .unwrap();
-    self.sockets.insert(tok, socket);
+    self.sockets.insert(tok, WrappedSocket::new(socket, plug_tx));
     self.serv_rx.insert(tok, serv_rx);
-    self.plug_tx.insert(tok, plug_tx);
     let plugin = Arc::new(plugin);
     self.plugins.push(plugin.clone());
     Some(plugin)
@@ -91,19 +97,27 @@ impl SocketManager {
     loop {
       self.poll.poll(&mut events, None).unwrap();
       for ev in &events {
-        /*
-        let mut sock = self.socket.lock();
-        let mut buf = vec![];
-        sock.read_until(b'\0', &mut buf).unwrap();
-        drop(sock);
-        buf.pop(); // Remove null byte
-        match serde_json::from_slice(&buf) {
-          Ok(v) => Ok(v),
-          Err(e) => {
-            error!("invalid event from plugin `{}`: {}", "", e);
-            Err(())
+        match ev.token() {
+          LISTEN => loop {
+            // Someone wants to send something to a plugin
+            let plugin_tok = match self.tok_rx.try_recv() {
+              Ok(tok) => tok,
+              Err(_) => break,
+            };
+            // Don't want to block here
+            let msg = self.serv_rx[&plugin_tok].try_recv().unwrap();
+
+            match self.sockets.get_mut(&plugin_tok).unwrap().send(msg) {
+              Ok(_) => {}
+              Err(e) => error!("error sending to plugin: {e}"),
+            }
+          },
+          token => {
+            // One of our sockets has just changed state
+            self.handle_socket_change(ev, token);
           }
         }
+        /*
 
         let mut sock = self.socket.lock();
         let mut data = serde_json::to_vec(&ev).unwrap();
@@ -113,6 +127,79 @@ impl SocketManager {
         Ok(())
         */
       }
+    }
+  }
+
+  fn handle_socket_change(&mut self, ev: &Event, tok: Token) -> io::Result<()> {
+    let socket = self.sockets.get_mut(&tok).unwrap();
+    if ev.is_readable() {
+      socket.try_read()?;
+    }
+    if ev.is_writable() {
+      socket.try_flush()?;
+    }
+    Ok(())
+  }
+}
+
+impl WrappedSocket {
+  pub fn new(stream: UnixStream, plug_tx: Sender<PluginEvent>) -> Self {
+    WrappedSocket {
+      stream,
+      outgoing: Vec::with_capacity(1024),
+      incoming: Vec::with_capacity(1024),
+      plug_tx,
+    }
+  }
+
+  pub fn send(&mut self, ev: ServerEvent) -> io::Result<()> {
+    self.outgoing.append(&mut serde_json::to_vec(&ev).unwrap());
+    self.outgoing.push(b'\0');
+    self.try_flush()?;
+    Ok(())
+  }
+
+  pub fn try_read(&mut self) -> io::Result<()> {
+    loop {
+      let mut buf = vec![0; 1024];
+      match self.stream.read(&mut buf) {
+        Ok(n) => {
+          self.incoming.extend(buf);
+          self.read_events()?;
+        }
+        Err(e) if matches!(e.kind(), io::ErrorKind::WouldBlock) => return Ok(()),
+        Err(e) => return Err(e),
+      }
+    }
+  }
+
+  fn read_events(&mut self) -> io::Result<()> {
+    while let Some(idx) = self.incoming.iter().position(|&x| x == b'\0') {
+      let buf = &self.incoming[..idx];
+
+      let res = serde_json::from_slice(&buf);
+      self.incoming.drain(..idx + 1);
+      match res {
+        Ok(v) => {
+          self.plug_tx.send(v).unwrap();
+        }
+        Err(e) => {
+          error!("invalid event from plugin `{}`: {}", "", e);
+          return Ok(());
+        }
+      }
+    }
+    Ok(())
+  }
+
+  pub fn try_flush(&mut self) -> io::Result<()> {
+    match self.stream.write(&self.outgoing) {
+      Ok(n) => {
+        self.outgoing.drain(..n);
+        Ok(())
+      }
+      Err(e) if matches!(e.kind(), io::ErrorKind::WouldBlock) => Ok(()),
+      Err(e) => return Err(e),
     }
   }
 }
