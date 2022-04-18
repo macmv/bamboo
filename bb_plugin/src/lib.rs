f1a96b877d1a79504d67487807ce7ffb467ad954
Neil Macneale <neil.macneale.v@gmail.com>
2022-04-18 01:29:55 -0700

    Sample was plugin can use bb_common Chat type

--- bb_plugin/src/lib.rs
+++ bb_plugin/src/lib.rs
@@ -1,22 +1,12 @@
 use bb_ffi::{CChat, CPlayer, CPos};
 use std::{ffi::CString, marker::PhantomData};
 
-pub struct Player {
-  ffi: CPlayer,
-}
-pub struct Chat {
-  message: String,
-}
-pub struct Pos {
-  ffi: CPos,
-}
+use bb_common::util::Chat;
+
+pub use bb_common::{math, util};
 
 pub struct Bamboo {
   marker: PhantomData<()>,
-}
-
-impl Chat {
-  pub fn new(message: String) -> Self { Chat { message } }
 }
 
 pub fn instance() -> Bamboo { Bamboo { marker: PhantomData::default() } }
@@ -24,7 +14,7 @@ pub fn instance() -> Bamboo { Bamboo { marker: PhantomData::default() } }
 impl Bamboo {
   pub fn broadcast(&self, message: Chat) {
     unsafe {
-      let s = CString::new(message.message.clone()).unwrap();
+      let s = CString::new(message.to_codes()).unwrap();
       let c_chat = CChat { message: s.as_ptr() };
       bb_ffi::broadcast(&c_chat);
     }
--- plugins/wasm-test/src/lib.rs
+++ plugins/wasm-test/src/lib.rs
@@ -1,8 +1,10 @@
-use bb_plugin::Chat;
+use bb_plugin::util::{chat::Color, Chat};
 
 #[no_mangle]
 extern "C" fn init(ret: &mut ()) {
   let msg = format!("Hello! number: 5");
+  let mut chat = Chat::new(msg);
+  chat.add("foo").color(Color::Red);
   let bb = bb_plugin::instance();
-  bb.broadcast(Chat::new(msg));
+  bb.broadcast(chat);
 }
