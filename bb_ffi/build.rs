e647203d35502ca17600b8a9b2f9ce8262deeb41
Neil Macneale <neil.macneale.v@gmail.com>
2022-04-18 00:57:40 -0700

    Added cbindgen to bb_ffi

--- bb_ffi/build.rs
+++ bb_ffi/build.rs
@@ -1,1 +1,11 @@
-fn main() {}
+use std::env;
+
+fn main() {
+  let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+
+  cbindgen::Builder::new()
+    .with_crate(crate_dir)
+    .generate()
+    .expect("Unable to generate bindings")
+    .write_to_file("bamboo.h");
+}
