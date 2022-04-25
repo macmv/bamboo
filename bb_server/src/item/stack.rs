fe4d6026dc9c0da4da2d36dcc7abb8a6cf9f7627
Neil Macneale <neil.macneale.v@gmail.com>
2022-04-24 22:39:06 -0700

    Added tests for item::Stack

--- bb_server/src/item/stack.rs
+++ bb_server/src/item/stack.rs
@@ -2,7 +2,7 @@ use super::Type;
 use bb_common::{nbt::NBT, util::Item};
 use std::num::NonZeroU8;
 
-#[derive(Clone, Debug, Eq)]
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Stack {
   item:   Type,
   amount: NonZeroU8,
@@ -29,12 +29,14 @@ impl Stack {
   /// Creates an item stack containing a single item with the given type.
   pub fn new(item: Type) -> Self { Stack { item, amount: ONE } }
 
-  /// Sets the amount in self, and returns the modified self.
+  /// Sets the amount in self, and returns the modified self. If the stack is
+  /// air, this will do nothing.
   pub fn with_amount(mut self, amount: u8) -> Self {
     self.set_amount(amount);
     self
   }
-  /// Sets the amount in the item stack.
+  /// Sets the amount in the item stack. If the stack is air, this will do
+  /// nothing.
   pub fn set_amount(&mut self, amount: u8) {
     if amount == 0 {
       self.item = Type::Air;
@@ -70,15 +72,40 @@ impl Stack {
   }
 }
 
-impl PartialEq for Stack {
-  /// If the amount is different for two `Air` items, this may be false. Note
-  /// that we always return `0` for `self.amount()` when this is air, so this
-  /// is mostly to avoid confusion.
-  fn eq(&self, other: &Stack) -> bool {
-    if self.item == Type::Air && other.item == Type::Air {
-      true
-    } else {
-      self.item == other.item && self.amount == other.amount
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_air() {
+    assert_eq!(Stack::empty(), Stack::new(Type::Air));
+    assert_eq!(Stack::empty(), Stack::new(Type::Air).with_amount(10));
+    assert_eq!(Stack::new(Type::Air), Stack::new(Type::Air).with_amount(10));
+    assert_eq!(Stack::new(Type::Air).with_amount(10).amount(), 0);
+    assert_eq!(Stack::new(Type::Stone).with_amount(0), Stack::empty());
+    assert_eq!(Stack::new(Type::Stone).with_amount(0).item(), Type::Air);
+    assert_eq!(Stack::new(Type::Stone).with_amount(0).amount(), 0);
+  }
+
+  #[test]
+  fn test_is_empty() {
+    assert!(Stack::empty().is_empty());
+    assert!(Stack::new(Type::Air).is_empty());
+    assert!(!Stack::new(Type::Stone).is_empty());
+    assert!(Stack::new(Type::Stone).with_amount(0).is_empty());
+  }
+
+  #[test]
+  fn test_item_convert() {
+    fn item_eq(stack: Stack, item: Item) {
+      assert_eq!(stack.to_item(), item);
+      assert_eq!(Stack::from(item), stack);
     }
+    item_eq(Stack::empty(), Item { id: 0, count: 0, damage: 0, nbt: NBT::empty("") });
+    item_eq(
+      Stack::new(Type::Air).with_amount(10),
+      Item { id: 0, count: 0, damage: 0, nbt: NBT::empty("") },
+    );
+    item_eq(Stack::new(Type::Stone), Item { id: 1, count: 1, damage: 0, nbt: NBT::empty("") });
   }
 }
