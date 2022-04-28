724d1ca941a98fd6924e2e3d571530da65b5ef4c
Neil Macneale <neil.macneale.v@gmail.com>
2022-04-28 14:58:13 -0700

    Fluids, items, and entity tags are expanded to the correct ids

--- bb_server/src/tags/mod.rs
+++ bb_server/src/tags/mod.rs
@@ -1,4 +1,4 @@
-use crate::block;
+use crate::{block, entity, item};
 use bb_common::net::cb;
 use std::{collections::HashMap, str::FromStr};
 
@@ -9,48 +9,82 @@ pub struct Tags {
   // TODO: Add custom tags here
 }
 
+#[derive(Debug, Clone, Copy)]
+enum TagKind {
+  Block,
+  Item,
+  Fluid,
+  Entity,
+  GameEvent,
+}
+
 impl Tags {
   #[allow(clippy::new_without_default)]
   pub fn new() -> Self { Tags { categories: generate_tags() } }
 
   pub fn serialize(&self) -> cb::Packet {
     cb::Packet::Tags {
-      block:       self.categories.block.serialize(),
-      item:        self.categories.item.serialize(),
-      fluid:       self.categories.fluid.serialize(),
-      entity_type: self.categories.entity_type.serialize(),
-      game_event:  self.categories.game_event.serialize(),
+      block:       self.categories.block.serialize(TagKind::Block),
+      item:        self.categories.item.serialize(TagKind::Item),
+      fluid:       self.categories.fluid.serialize(TagKind::Fluid),
+      entity_type: self.categories.entity_type.serialize(TagKind::Entity),
+      game_event:  self.categories.game_event.serialize(TagKind::GameEvent),
     }
   }
 }
 
 impl TagCategory {
-  fn serialize(&self) -> HashMap<String, Vec<i32>> {
+  fn serialize(&self, kind: TagKind) -> HashMap<String, Vec<i32>> {
     self
       .tags
       .iter()
       .map(|tag| {
         (
           tag.name.strip_prefix('#').unwrap().to_string(),
-          tag.values.iter().copied().flat_map(|elem| self.expand_tag(elem)).collect(),
+          tag.values.iter().copied().flat_map(|elem| self.expand_tag(elem, kind)).collect(),
         )
       })
       .collect()
   }
 
-  fn expand_tag(&self, name: &str) -> Vec<i32> {
+  fn expand_tag(&self, name: &str, kind: TagKind) -> Vec<i32> {
     if name.starts_with('#') {
       for tag in self.tags {
         if name == tag.name {
-          return tag.values.iter().copied().flat_map(|elem| self.expand_tag(elem)).collect();
+          return tag.values.iter().copied().flat_map(|elem| self.expand_tag(elem, kind)).collect();
         }
       }
       panic!();
     } else {
-      match block::Kind::from_str(name) {
+      kind.expand_str(name)
+    }
+  }
+}
+
+impl TagKind {
+  pub fn expand_str(&self, name: &str) -> Vec<i32> {
+    match self {
+      Self::Block => match block::Kind::from_str(name) {
         Ok(id) => vec![id as i32],
         Err(_) => vec![],
-      }
+      },
+      Self::Item => match item::Type::from_str(name) {
+        Ok(id) => vec![id as i32],
+        Err(_) => vec![],
+      },
+      Self::Entity => match entity::Type::from_str(name) {
+        Ok(id) => vec![id as i32],
+        Err(_) => vec![],
+      },
+      Self::Fluid => match name {
+        // fuild 0 is `empty`
+        "water" => vec![1],
+        "flowing_water" => vec![2],
+        "lava" => vec![3],
+        "flowing_lava" => vec![4],
+        _ => panic!("invalid fluid {name}"),
+      },
+      Self::GameEvent => vec![],
     }
   }
 }
