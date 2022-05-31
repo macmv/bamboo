5109d3e450c6ed4054f98b294e8e57715d6e622a
Neil Macneale <neil.macneale.v@gmail.com>
2022-05-30 18:23:41 -0700

    Documented bb_plugin_macros::behavior

--- bb_plugin_macros/src/lib.rs
+++ bb_plugin_macros/src/lib.rs
@@ -10,6 +10,32 @@ pub fn define_ty(args: TokenStream, input: TokenStream) -> TokenStream {
   define_ty::define_ty(args, input)
 }
 
+/// Creates a match statement for item/block behaviors. This is only for builtin
+/// behaviors, and doesn't include plugins.
+///
+/// The syntax is as follows (comments are ignored):
+///
+/// ```
+/// bb_plugin_macros::behavior! {
+///   // Sets the base enum name for everything. `Kind::` will be appended to the mappings below.
+///   :Kind:
+///
+///   // Defines a mapping. This says `Kind::Chest` should return a `Box::new(impls::Chest)`. The
+///   // right side is any expression.
+///   Chest => impls::Chest;
+///
+///   // Defines a mapping that matches `Kind::Sand | Kind::RedSand ...` to `Box::new(impls::Falling)`.
+///   Sand | RedSand | Gravel => impls::Falling;
+///
+///   // Defines a set. This is a collection of other items.
+///   *wood* = Oak, Birch, Spruce, DarkOak, Acacia, Jungle;
+///
+///   // Defins a mapping. This will map all possible concatenations of the `wood` set, appended
+///   // with `Log`. So this will match `Kind::OakLog | Kind::BirchLog | Kind::SpruceLog ...` to
+///   // `Box::new(impls::Log)`.
+///   *wood*Log => impls::Log;
+/// };
+/// ```
 #[proc_macro]
 pub fn behavior(input: TokenStream) -> TokenStream { behavior::behavior(input) }
 
