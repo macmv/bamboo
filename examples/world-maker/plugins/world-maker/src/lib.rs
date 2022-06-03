a7d9df35dcd98231de8cbb3b7d38b13923bd1f2b
Neil Macneale <neil.macneale.v@gmail.com>
2022-06-03 14:34:15 -0700

    A single particle is now shown, instead of 8000

--- examples/world-maker/plugins/world-maker/src/lib.rs
+++ examples/world-maker/plugins/world-maker/src/lib.rs
@@ -36,26 +36,19 @@ fn on_tick() {
   let world = bb_plugin::world::World::new(0);
   for player in world.players() {
     let pos = player.pos();
-    // let look = player.look_as_vec();
-    for x in -10..10 {
-      for y in -10..10 {
-        for z in -10..10 {
-          let look = FPos::new(x as f64 / 10.0, y as f64 / 10.0, z as f64 / 10.0);
-          let from = pos + FPos::new(0.0, 1.5, 0.0);
-          let to = from + FPos::new(look.x * 50.0, look.y * 50.0, look.z * 50.0);
+    let look = player.look_as_vec();
+    let from = pos + FPos::new(0.0, 1.5, 0.0);
+    let to = from + FPos::new(look.x * 50.0, look.y * 50.0, look.z * 50.0);
 
-          if let Some(pos) = world.raycast(from, to, true) {
-            player.send_particle(Particle {
-              ty: particle::Type::Dust(Color { r: 255, g: 255, b: 255 }, 0.5),
-              pos,
-              offset: FPos::new(0.0, 0.0, 0.0),
-              count: 1,
-              data: 0.0,
-              long_distance: false,
-            });
-          }
-        }
-      }
+    if let Some(pos) = world.raycast(from, to, true) {
+      player.send_particle(Particle {
+        ty: particle::Type::Dust(Color { r: 255, g: 255, b: 255 }, 0.5),
+        pos,
+        offset: FPos::new(0.0, 0.0, 0.0),
+        count: 1,
+        data: 0.0,
+        long_distance: false,
+      });
     }
   }
 }
