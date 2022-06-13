7edcf76b33a98babbe498f37a0d4e0d5280e4695
Neil Macneale <neil.macneale.v@gmail.com>
2022-06-13 16:45:34 -0700

    Electric floor compiles, and crashes

--- examples/electric-floor/plugins/electric-floor/src/lib.rs
+++ examples/electric-floor/plugins/electric-floor/src/lib.rs
@@ -2,31 +2,24 @@
 extern crate bb_plugin;
 
 use bb_plugin::{
-  block,
-  command::{Arg, Command, Parser},
-  math::{FPos, Pos},
-  particle,
-  particle::{Color, Particle},
+  command::{Arg, Command},
+  math::Pos,
   player::Player,
-  PlayerStore,
 };
-use std::{
-  any::Any,
-  sync::atomic::{AtomicBool, Ordering},
-};
+use std::sync::atomic::{AtomicBool, AtomicU32, Ordering};
 
 const MIN: Pos = Pos::new(-32, 80, -32);
 const MAX: Pos = Pos::new(32, 80, 32);
 
-static STARTED: AtomicBool = false.into();
-static TICK: AtomicU32 = 0.into();
+static STARTED: AtomicBool = AtomicBool::new(false);
+static TICK: AtomicU32 = AtomicU32::new(0);
 
 #[no_mangle]
 extern "C" fn init() {
   bb_plugin::init();
   bb_plugin::set_on_tick(on_tick);
   let cmd = Command::new("start");
-  bb_plugin::add_command(cmd, cmd_start);
+  bb_plugin::add_command(&cmd, cmd_start);
 }
 
 fn cmd_start(_player: Option<Player>, _args: Vec<Arg>) { STARTED.store(true, Ordering::SeqCst); }
@@ -39,10 +32,9 @@ fn on_tick() {
   if t % 10 == 0 {
     let world = bb_plugin::world::World::new(0);
     for pos in MIN.to(MAX) {
-      let ty = world.get_block(pos);
-      if ty.prop("stage") != 0 && ty.prop("stage") < 5 {
-        ty.set_prop("stage", ty.prop("stage").int() + 1);
-        world.set_block(pos, ty);
+      let ty = world.get_block(pos).unwrap();
+      if ty.prop("stage") != 0 && ty.prop("stage").int() < 5 {
+        world.set_block(pos, ty.with_prop("stage", ty.prop("stage").int() + 1));
       }
     }
   }
