f38c1ad9da711e4ec67ea125f3293246d5e437e9
Neil Macneale <neil.macneale.v@gmail.com>
2022-06-26 21:08:10 -0700

    Fix doctests, although the output is full of ignored garbage

--- bb_proxy/src/gnet/mod.rs
+++ bb_proxy/src/gnet/mod.rs
@@ -1,25 +1,14 @@
 // data::generate_protocol!();
 
-#[cfg(not(doctest))]
 pub mod cb {
   use super::*;
 
   include!(concat!(env!("OUT_DIR"), "/protocol/cb.rs"));
 }
-#[cfg(not(doctest))]
 pub mod sb {
   use super::*;
 
   include!(concat!(env!("OUT_DIR"), "/protocol/sb.rs"));
-}
-
-#[cfg(doctest)]
-pub mod cb {
-  pub enum Packet {}
-}
-#[cfg(doctest)]
-pub mod sb {
-  pub enum Packet {}
 }
 
 mod other;
