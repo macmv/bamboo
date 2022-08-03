d418abce2a9e2ef508433b3ccab0129033091dff
Neil Macneale <neil.macneale.v@gmail.com>
2022-08-03 16:29:36 -0700

    Falling blocks will now drop correctly, once hitboxes are correct

--- bb_server/src/entity/behavior/falling_block.rs
+++ bb_server/src/entity/behavior/falling_block.rs
@@ -1,5 +1,5 @@
 use super::{Behavior, EntityData, EntityPos, ShouldDespawn};
-use crate::world::World;
+use crate::{block, entity, entity::Metadata, item, item::Stack, world::World};
 use bb_common::math::FPos;
 use std::sync::Arc;
 
@@ -14,7 +14,7 @@ impl Behavior for FallingBlock {
     // 0.03 b/tick for projectiles, 0.04 b/tick for items, and 0.08 b/tick for
     // living entities.
     if !p.grounded {
-      p.vel.y -= 0.08;
+      p.vel.y -= 0.04;
     }
     p.vel.y *= 0.98;
 
@@ -30,8 +30,18 @@ impl Behavior for FallingBlock {
       // This new position is at the center of the block, which will be the most
       // accurate for converting to a block position. If we don't add 0.5, then chains
       // of falling blocks can sometimes overlap.
-      let _ = world.set_block((p.aabb.pos + FPos::new(0.0, 0.5, 0.0)).block(), ty);
-      ShouldDespawn(true)
+      let block_pos = (p.aabb.pos + FPos::new(0.0, 0.5, 0.0)).block();
+      let kind = world.get_kind(block_pos).unwrap_or(block::Kind::Air);
+      if kind == block::Kind::Air || world.block_converter().get(kind).material.is_replaceable() {
+        let _ = world.set_block(block_pos, ty);
+        ShouldDespawn(true)
+      } else {
+        let mut meta = Metadata::new();
+        meta.set_item(8, Stack::new(item::Type::Sand).to_item());
+        world.summon_meta(entity::Type::Item, block_pos.center(), meta);
+
+        ShouldDespawn(true)
+      }
     } else {
       ShouldDespawn(false)
     }
