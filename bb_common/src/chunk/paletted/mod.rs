8b96d3905b2dd8c005fee9222d012ce614d00309
Neil Macneale V <neil.macneale.v@gmail.com>
2023-02-05 22:31:43 -0800

    Add PaletteSection::palette_mut

--- bb_common/src/chunk/paletted/mod.rs
+++ bb_common/src/chunk/paletted/mod.rs
@@ -128,6 +128,10 @@ impl Section {
   }
   /// Returns the palette of this chunk.
   pub fn palette(&self) -> &[u32] { &self.palette }
+  /// Returns a mutable reference to the palette of this chunk.
+  ///
+  /// For the chunk to be usable, this must be kept in order.
+  pub fn palette_mmut(&mut self) -> &mut [u32] { &mut self.palette }
   // Returns the number of non air blocks in this chunk. Because paletted chunks
   // track all the amounts of blocks within the chunk, this is a single Vec
   // lookup.
