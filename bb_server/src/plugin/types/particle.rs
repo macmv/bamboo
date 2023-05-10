dab41c601139b64c13f98c0cdbef5c6880585a0d
snavy <1337snavy@gmail.com>
2023-05-10 04:54:03 +0000

    remove unnecessary particle constructor

--- bb_server/src/plugin/types/particle.rs
+++ bb_server/src/plugin/types/particle.rs
@@ -1,10 +1,10 @@
 use panda::{
-    parse::token::Span,
-    runtime::RuntimeError,
+  parse::token::Span,
+  runtime::RuntimeError,
 };
 use crate::{
-    particle,
-    particle::Particle,
+  particle,
+  particle::Particle,
 };
 use bb_server_macros::define_ty;
 use crate::plugin::types::util::PFPos;
@@ -12,25 +12,25 @@ use std::str::FromStr;
 
 #[define_ty]
 impl PParticle {
-    info! {
-        wrap: Particle,
+  info! {
+    wrap: Particle,
 
-        panda: {
-            path: "bamboo::particle::Particle",
-        },
-    }
+    panda: {
+      path: "bamboo::particle::Particle",
+    },
+  }
 
-    pub fn new(name: &str, pos: &PFPos, force: bool, offset: &PFPos, count: u32, data: f32) -> Result<Self, RuntimeError> {
-        Ok(PParticle {
-            inner: Particle::new(
-                particle::Type::from_str(name)
-                    .map_err(|e| RuntimeError::Custom(e.to_string(), Span::call_site()))?,
-                pos.inner,
-                force,
-                offset.inner,
-                count,
-                data,
-            ),
-        })
-    }
+  pub fn new(name: &str, pos: &PFPos, force: bool, offset: &PFPos, count: u32, data: f32) -> Result<Self, RuntimeError> {
+    Ok(PParticle {
+      inner: Particle {
+        ty: particle::Type::from_str(name)
+          .map_err(|e| RuntimeError::Custom(e.to_string(), Span::call_site()))?,
+        pos: pos.inner,
+        long_distance: force,
+        offset: offset.inner,
+        count,
+        data,
+      },
+    })
+  }
 }
