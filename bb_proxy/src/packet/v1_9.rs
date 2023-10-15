20d5c475f81ade3b184faeda9bd64e2e67c3dad0
Neil Macneale V <neil.macneale.v@gmail.com>
2023-10-15 00:25:07 -0700

    Can serialize lighting data to 1.9-1.12 clients

--- bb_proxy/src/packet/v1_9.rs
+++ bb_proxy/src/packet/v1_9.rs
@@ -8,31 +8,60 @@ pub fn chunk(chunk: ChunkWithPos, ver: ProtocolVersion, conv: &TypeConverter) ->
   let skylight = true; // Assume overworld
 
   let base = 16 * 16 / 2;
-  let mut chunk_data = Vec::with_capacity(
-    1024 + chunk.sections.iter().flatten().count() * (19 + 16 * 16 * 16 + 16 * base),
-  );
+  let total_sections = chunk
+    .sections
+    .iter()
+    .zip(chunk.block_light.sections().iter())
+    .filter(|(s, l)| s.is_some() || l.is_some())
+    .count();
+  let mut chunk_data = Vec::with_capacity(1024 + total_sections * (19 + 16 * 16 * 16 + 16 * base));
   let mut chunk_buf = Buffer::new(&mut chunk_data);
-  for s in chunk.sections.iter().flatten() {
-    chunk_buf.write_u8(s.data().bpe());
-    chunk_buf.write_varint(s.palette().len() as i32);
-    for g in s.palette() {
-      chunk_buf.write_varint(conv.block_to_old(*g, ver.block()) as i32);
+  for (block_section, light_section) in
+    chunk.sections.iter().zip(chunk.block_light.sections().iter())
+  {
+    if block_section.is_none() && light_section.is_none() {
+      continue;
     }
-    let longs = s.data().old_long_array();
-    chunk_buf.write_varint(longs.len() as i32);
-    chunk_buf.reserve(longs.len() * 8); // 8 bytes per long
-    longs.iter().for_each(|v| chunk_buf.write_buf(&v.to_be_bytes()));
-    // Light data
-    chunk_buf.reserve(16 * base);
-    for _ in 0..16 * 16 * 16 / 2 {
+
+    if let Some(s) = block_section {
+      chunk_buf.write_u8(s.data().bpe());
+      chunk_buf.write_varint(s.palette().len() as i32);
+      for g in s.palette() {
+        chunk_buf.write_varint(conv.block_to_old(*g, ver.block()) as i32);
+      }
+      let longs = s.data().old_long_array();
+      chunk_buf.write_varint(longs.len() as i32);
+      chunk_buf.reserve(longs.len() * 8); // 8 bytes per long
+      longs.iter().for_each(|v| chunk_buf.write_buf(&v.to_be_bytes()));
+    } else {
+      // Write an empty section
+      chunk_buf.write_u8(4); // 4 bits per entry
+      chunk_buf.write_varint(1); // Palette has 1 entry
+      chunk_buf.write_varint(0); // The entry is `0`
+
+      // each entry is 4 bits
+      chunk_buf.write_varint(256); // there are 256 longs (16 * 16 * 16 / 2 / 8)
+      chunk_buf.reserve(16 * 16 * 16 / 2);
+      for _ in 0..16 * 16 * 16 / 2 {
+        chunk_buf.write_u8(0x00);
+      }
+    }
+    // Block light data
+    if let Some(l) = light_section {
+      chunk_buf.write_buf(l.data());
+    } else {
       // Each lighting value is 1/2 byte
-      chunk_buf.write_u8(0xff);
+      chunk_buf.reserve(16 * 16 * 16 / 2);
+      for _ in 0..16 * 16 * 16 / 2 {
+        chunk_buf.write_u8(0x00);
+      }
     }
+
     if skylight {
       chunk_buf.reserve(16 * base);
       for _ in 0..16 * 16 * 16 / 2 {
         // Each lighting value is 1/2 byte
-        chunk_buf.write_u8(0xff);
+        chunk_buf.write_u8(0x00);
       }
     }
   }
